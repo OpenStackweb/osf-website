c1d807e04ab986ccbb24aa5b14173dc326c83341
Tomás Castillo <tcastilloboireau@gmail.com>
2021-08-14 11:23:24 -0300

    new join page WIP
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/templates/join-page.js
+++ src/templates/join-page.js
@@ -11,6 +11,8 @@ import SEO from '../components/SEO';
 import envVariables from '../utils/envVariables'
 import { connect } from "react-redux";
 
+import leftArrow from '../img/svg/arrow-left.svg'
+
 export const JoinPageTemplate = ({
     isLoggedUser,
     title,
@@ -26,7 +28,140 @@ export const JoinPageTemplate = ({
             <div className="wrapper project-background">
                 <TopBar />
                 <Navbar isLoggedUser={isLoggedUser} />
-                <Header title={title} subTitle={subTitle} />
+                <div className="join-header">
+                    <div className="header-right">
+                        <span className="upper-title">
+                            Join the openinfra foundation
+                        </span>
+                        <span className="title">
+                            Join Us In Advancing Open InfrastructurE
+                        </span>
+                        <span className="description">
+                            The success of OpenInfra project communities relies on the support of OpenInfra
+                            Foundation members who take an active role in helping achieve the OpenInfra
+                            Foundation mission: Build open source communities who write software that
+                            runs in production.
+                        </span>
+                        <span className="join">
+                            Join as:
+                        </span>
+                        <div className="buttons">
+                            <a>A Business <img src={leftArrow} alt="" /> </a>
+                            <a>An Individual <img src={leftArrow} style={{ fill: "#848484" }} alt="" /> </a>
+                        </div>
+                    </div>
+                    <div className="header-left">
+                        <div className="picture">
+                            <img src="/img/join/EWP-IMG_8197 1.jpg" />
+                            <svg width="663" height="520" viewBox="0 0 663 520" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect width="42.9841" height="125.255" rx="21.4921" transform="matrix(-0.707107 0.707107 0.707107 0.707107 520.523 200.929)" fill="#F7B749" />
+                                <rect y="125.451" width="155.711" height="453.74" rx="77.8557" transform="rotate(-45 0 125.451)" fill="#43B85C" />
+                                <rect x="51.3923" y="223.999" width="43.3922" height="126.444" rx="21.6961" transform="rotate(-45 51.3923 223.999)" fill="#43B85C" />
+                                <rect x="143.708" y="132.796" width="187.633" height="546.757" rx="93.8163" transform="rotate(-45 143.708 132.796)" fill="#F7B749" />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                <div className="waoi">
+                    <div className="waoi-left">
+                        <div>
+                            <span className="waoi-left-number">110,000+</span>
+                            <span className="waoi-left-text">community members</span>
+                        </div>
+                        <div>
+                            <span className="waoi-left-number">560+</span>
+                            <span className="waoi-left-text">supporting organizations</span>
+                        </div>
+                        <div>
+                            <span className="waoi-left-number">10,000+</span>
+                            <span className="waoi-left-text">developers</span>
+                        </div>
+                        <div>
+                            <span className="waoi-left-number">180+</span>
+                            <span className="waoi-left-text">countries</span>
+                        </div>
+                    </div>
+                    <div className="waoi-right">
+                        <span className="waoi-right-title">#WeAreOpenInfra</span>
+                        <br />
+                        <span className="waoi-right-text">
+                            OpenInfra is more than the open source software that powers the most mission critical
+                            infrastructure in the world. OpenInfra is the community that consists of developers,
+                            users, operators, administrators, and contributors from all parts of the globe, working
+                            in an incredible range of industries from transportation to telco, gaming to retail,
+                            research to education and everything in between.
+                        </span>
+                    </div>
+                </div>
+                <div className="communities">
+                    <span className="communities-title">
+                        We build communities who write software that runs in production
+                    </span>
+                    <div className="communities-text">
+                        <div>
+                            Projects that call OpenInfra Foundation their home each value open collaboration
+                            and exemplify the Four Opens (open source, design, development, community).
+                            All of our projects have a strategic focus, vision & scope that furthers the
+                            OpenInfra Foundation mission of supporting the development and adoption of
+                            production infrastructure with open source components.
+                        </div>
+                        <div>
+                            We value project individuality and collaboration. The quality of each project is
+                            our main focus, not the overall quantity of projects we host. Our model provides
+                            the support system and collaborative environment where communities are able to focus
+                            their attention on the important work of building and operating software that solves
+                            real problems around the globe.
+                        </div>
+                    </div>
+                    <div className="communities-icons">
+                        <div class="section-bg-green-logos-container">
+                            <div class="section-bg-green-logo">
+                                <img src="/img/home/logo1.svg" alt="" />
+                            </div>
+                            <div class="section-bg-green-logo">
+                                <img src="/img/home/logo2.svg" alt="" />
+                            </div>
+                            <div class="section-bg-green-logo">
+                                <img src="/img/home/logo7.svg" alt="" />
+                            </div>
+                            <div class="section-bg-green-logo">
+                                <img src="/img/home/logo3.svg" alt="" />
+                            </div>
+                            <div class="section-bg-green-logo">
+                                <img src="/img/home/logo4.svg" alt="" />
+                            </div>
+                            <div class="section-bg-green-logo">
+                                <img src="/img/home/logo5.svg" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="contact">
+                    <img src="/img/apple-touch-icon.png" alt="" />
+                    <span>Interested in hosting your open source project with the OpenInfra Foundation?</span>
+                    <a>Contact Us </a>
+                </div>
+                <div className="involved">
+                    <span className="involved-title">
+                        Get Involved
+                    </span>
+                    <div className="involved-slide">
+                        <div className="gradient" />
+                        <img src="/img/join/ossvan_hallway_convo-7 1.png" />
+                        <img src="/img/join/EWP-IMG_1870 1.png" />
+                        <img src="/img/join/Tokyo WOO 1.png" />
+                        <img src="/img/join/marketmixer_full-111 1.png" />
+                        <img src="/img/join/Barcelona Attendees 1.png" />
+                        <img src="/img/join/OSSBER_Keynote_web-32 1.png" />
+                    </div>
+                    <div className="involved-buttons">
+                        <span>Join as:</span>
+                        <div className="buttons-container">
+                            <a>A Business <img src={leftArrow} alt="" /> </a>
+                            <a>An Individual <img src={leftArrow} alt="" /> </a>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <main className="main">
@@ -87,7 +222,7 @@ const JoinPage = ({ isLoggedUser, data }) => {
 
     return (
         <Layout>
-            <SEO seo={post.frontmatter.seo ? post.frontmatter.seo : null} />            
+            <SEO seo={post.frontmatter.seo ? post.frontmatter.seo : null} />
             <JoinPageTemplate
                 isLoggedUser={isLoggedUser}
                 contentComponent={HTMLContent}

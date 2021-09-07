55769c03ce24b8247c9dc8f64cfbf9fadc4d5550
Wes Wilson <wes@iamweswilson.com>
2021-09-07 15:10:24 -0700

    live - keynotes promo banner

--- src/templates/openinfra-live.js
+++ src/templates/openinfra-live.js
@@ -9,6 +9,7 @@ import Navbar from '../components/Navbar';
 import SEO from '../components/SEO'
 import Hero from '../components/Hero'
 import ColorBar from '../img/color-bar.png'
+import leftArrow from '../img/svg/arrow-left.svg'
 
 import { connect } from "react-redux";
 
@@ -139,6 +140,28 @@ export const OpenInfraLiveTemplate = ({
                 </div>
               </div>
             </section>
+            <div className="ideas-banner keynotes-promo">
+              <div>
+                <h2>
+                  Join us for OpenInfra Live: Keynotes!
+                </h2>
+                <p>
+                  Exclusive Announcements. Live Demos. OpenStack + Kubernetes. Hybrid Cloud Economics.
+                </p>
+                <p className="dates">
+                  November 17 and 18 at 9am CT/1500 UTC
+                  <a className="learn-link" href="/live/keynotes">
+                    Learn More
+                  </a>
+                </p>
+              </div>
+              <div>
+                <a href="https://openinfralivekeynotes.eventbrite.com/">
+                  Register Now <img src={leftArrow} alt="" />
+                </a>
+              </div>
+              <img src={ColorBar} className="multi-color-border" alt="Color Bar" />
+            </div>
           </div>
           <section className="live-section">
             <div className="container">

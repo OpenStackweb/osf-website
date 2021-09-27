686c2f43c33cd577bd7501b3f310de9b1134e13d
Tomás Castillo <tcastilloboireau@gmail.com>
2021-09-27 17:46:37 -0300

    new plugin google-tagmanager

--- gatsby-config.js
+++ gatsby-config.js
@@ -163,7 +163,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "UA-139234657-1", // Google Analytics / GA          
+          "UA-139234657-1", // Google Analytics / GA,
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
@@ -180,6 +180,39 @@ module.exports = {
           // Avoids sending pageview hits from custom paths
           // exclude: ["/preview/**", "/do-not-track/me/too/"],
         },
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-5SLZBPV",
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: true,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+  
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+  
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+
+        // Defaults to false
+        enableWebVitalsTracking: true,
+
+        // Defaults to https://www.googletagmanager.com
+        // selfHostedOrigin: "YOUR_SELF_HOSTED_ORIGIN",
       },
     },
     {

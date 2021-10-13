22e5821b9d3f81830b16cbe8f3acb87f8dae4fb3
James Cole <james@openstack.org>
2021-10-13 10:26:32 -0500

    Update text
    Signed-off-by: James Cole <james@openstack.org>

--- src/components/KeynotesPromoV2.js
+++ src/components/KeynotesPromoV2.js
@@ -15,7 +15,7 @@ class KeynotesPromoV2 extends React.Component {
 
                     <span className="upper-title">JOIN US FOR</span>
                     <h1 className="title">OpenInfra Live: Keynotes</h1>
-                    <p className="promo-para">This is our only big event for the year!</p>
+                    <p className="promo-para">Exclusive Announcements. Live Demos. OpenStack + Kubernetes. Hybrid Cloud Economics.</p>
                     <div className="dates-row">
                       <img className="cal-icon" src={CalIcon} /> 
                       <p className="promo-dates"> November 17 and 18 at 9am CT/1500 UTC</p>

a3e54e50e83d0d4d60d3920b833750e57e84be7d
Tomás Castillo <tcastilloboireau@gmail.com>
2021-10-29 11:03:29 -0300

    testing headers in ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,4 @@
+import React from "react"
 import { SSRWrapper } from "./src/state/ReduxWrapper"
 // @see https://www.gatsbyjs.com/docs/adding-redux-store/
 export const wrapRootElement = SSRWrapper;
@@ -21,3 +22,54 @@ global.window.matchMedia = function () {
 
 global.Blob = Blob
 global.XMLHttpRequest = XMLHttpRequest
+
+const isProduction = process.env.NODE_ENV === "production";
+
+console.log('is production?', isProduction);
+
+export const onPreRenderHTML = (args, pluginOptions) => {
+    const { blockGtm = true, manualMode = true, includeInDevelopment = true, } = pluginOptions;
+    // Do not modify scripts when in development. Can be overriden with plugin options.
+    if (!isProduction || !includeInDevelopment)
+        return;
+    const { getHeadComponents, replaceHeadComponents, getPreBodyComponents, replacePreBodyComponents, } = args;
+    const headComponents = getHeadComponents();
+    // Headcomponents needs to be assigned to a new
+    const newHeadComponents = headComponents.map((component) => {
+        if (component.type === "script") {
+            if (component.key === "plugin-google-tagmanager" &&
+                manualMode &&
+                blockGtm &&
+                isProduction // gatsby-plugin-google-tagmanager will thrown an error if the script has not been loaded in development
+            ) {
+                // Add Cookiebot manual mode data attribute to GTM script
+                return (React.createElement("script", Object.assign({ "data-cookieconsent": "statistics", type: "text/plain", key: component.key }, component.props)));
+            }
+        }
+        return component;
+    });
+    replaceHeadComponents(newHeadComponents);
+    const preBodyComponents = getPreBodyComponents();
+    const newPreBodyComponents = preBodyComponents.map((component) => {
+        if (component.type === "noscript") {
+            if (component.key === "plugin-google-tagmanager" &&
+                manualMode &&
+                blockGtm &&
+                isProduction // gatsby-plugin-google-tagmanager will thrown an error if the script has not been loaded in development
+            ) {
+                // Add Cookiebot manual mode data attribute to GTM noscript's iframe script
+                const gtmIframeStr = component.props.dangerouslySetInnerHTML.__html;
+                // Add data attribute to string
+                const gtmIframeStrWithCookiebotManualMode = gtmIframeStr.substr(0, 8) +
+                    'data-cookieconsent="statistics" ' +
+                    gtmIframeStr.substr(8);
+                const newProps = Object.assign({}, component.props);
+                newProps.dangerouslySetInnerHTML.__html =
+                    gtmIframeStrWithCookiebotManualMode;
+                return React.createElement("noscript", Object.assign({ key: component.key }, newProps));
+            }
+        }
+        return component;
+    });
+    replacePreBodyComponents(newPreBodyComponents);
+};

07a01b7874f2426f2228e544e4ea6dc6e7bb8af5
Tomás Castillo <tcastilloboireau@gmail.com>
2021-11-09 19:05:30 -0300

    Add hybrid cloud page
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

---
+++ src/pages/hybrid-cloud/index.md
@@ -0,0 +1,151 @@
+---
+templateKey: hybrid-cloud-page
+seo:
+  description: >-
+    An Open Community balances the roles of open source developers, users and
+    ecosystem, ensuring the community is a cohesive, inclusive, level playing
+    ground. 
+  image: /img/OpenInfra-icon-white.jpg
+  title: Open Source Project Community
+  twitterUsername: '@OpenInfraDev'
+  url: 'https://openinfra.dev/hybrid-cloud/'
+title: Hybrid Cloud
+subTitle: >-
+  Subhead goes here
+introduction: >-
+  Open infrastructure companies choosing multicloud or hybrid systems cannot rely on 
+  proprietary tools from a single vendor but instead must run standard software 
+  everywhere—and OSS best meets these requirements
+useCases: 
+  display: true
+  title: Hybrid Cloud Use Case Powered by Open Infrastructure
+  cases:
+    - image: '/img/hybrid-cloud/case-cash.svg'
+      backgroundColor: "#43B85C"
+      text: 'The hybrid cloud market was valued at <b>USD 52 billion</b> in 2020 and is expected to reach <b>USD 145 
+      billion</b> by 2026.'
+      source: "Source: Hybrid Cloud Market - Growth, Trends, COVID-19 Impact, and Forecasts, Mordor Intelligence"
+    - image: '/img/hybrid-cloud/case-cloud.svg'
+      backgroundColor: "#2CB4E2"
+      text: 'When choosing cloud providers, <b>70% of respondents prefer one based on open source</b>.'
+      source: "Source: Hybrid Cloud Market - Growth, Trends, COVID-19 Impact, and Forecasts, Mordor Intelligence"
+benefits:
+  display: true
+  title: Key Benefits
+  items:
+    - image: '/img/hybrid-cloud/benefit-workload.svg'
+      title: Workload Flexibility
+    - image: '/img/hybrid-cloud/benefit-cost.svg'
+      title: Cost Savings
+    - image: '/img/hybrid-cloud/benefit-latency.svg'
+      title: Low Latency
+    - image: '/img/hybrid-cloud/benefit-security.svg'
+      title: Security
+    - image: '/img/hybrid-cloud/benefit-scalability.svg'
+      title: Scalability 
+projects:
+  title: Projects
+  display: true
+  list:     
+    - image: '/img/hybrid-cloud/Logo-Openstack.png'
+      bullets:
+        - bullet: 'OpenStack is the de facto private cloud; capable, tested, and proven at massive scale
+        spanning 25 million cores deployed by organizations across every industry.'
+        - bullet: 'The OpenStack-powered public cloud footprint has grown to 175 global data centers.'
+        - bullet: '<a href="#">Case study links to come</a>'
+    - image: '/img/hybrid-cloud/Logo-KataContainers.png'
+      bullets:
+        - bullet: 'Confidential Computing has the potential to accelerate the adoption of hybrid cloud 
+        computing for the highly-sensitive industries of finance, healthcare, insurance, or any business
+        concerned with the migration of data and workloads to the public cloud.'
+        - bullet: 'IBM integrated Kata Containers as a Kubernetes runtime class used for pods where they need stronger guarantees for running arbitrary untrusted workloads.: <a href="https://www.ibm.com/blogs/research/2020/11/amd-sev-ibm-hybrid-cloud/">
+      https://www.ibm.com/blogs/research/2020/11/amd-sev-ibm-hybrid-cloud/</a>'
+    - image: '/img/hybrid-cloud/Logo-Airship.png'
+      bullets: 
+        - bullet: 'In the Airship 2.0 release, the project moved to a hybrid and multi-cloud architecture 
+        using Kubernetes as the portability layer.'
+        - bullet: 'Download: <a href="https://www.airshipit.org/blog/airship2-released/">
+        https://www.airshipit.org/blog/airship2-released/</a>'
+    - image: '/img/hybrid-cloud/Logo-StarlingX.png'
+      bullets:
+        - bullet: 'StarlingX provides a container-based infrastructure for edge implementations in scalable 
+        solutions, providing low latency required by edge applications, and tools that make distributed 
+        edge—an emerging hybrid cloud use case—in production a reality.' 
+    - image: '/img/hybrid-cloud/Logo-Zuul.png'
+      bullets:
+        - bullet: 'Zuul schedules jobs in any arbitrary cloud it can login to, and the community has recently 
+        integrated drivers for AWS, Azure, GCE, and OpenStack cloud environments.' 
+        - bullet: '<a href="#">Link coming soon </a>'
+organizations:
+  display: true
+  title: Organizations
+  list:
+    - image: /img/hybrid-cloud/org-99cloud.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-awcloud.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-binero.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-bluvalt.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-cdac.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-china-mobile.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-china-telecom.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-citynetwork.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-cloudfire.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-DeutscheTelekom.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-devstack.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-easystack.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-elastx.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-exaion.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-ibm.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-infomaniak.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-inmotion.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-mirantis.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-nipa-cloud.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-orock.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-osism.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-ovhcloud.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-rackspace.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-sardina.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-stackhpc.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-stc.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-tencent-cloud.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-ubuntu.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-vexxhost.png
+      link: '#'
+    - image: /img/hybrid-cloud/org-vmware.png
+      link: '#'
+footer:
+  button: '#'
+  buttonText: Contact Us
+  display: true
+  title: Is your organization building an OpenInfra hybrid cloud strategy?
+  subTitle: >-
+    Learn how you can join the Foundation to help build the open source communities who 
+    are writing software that runs in production.
+---
---
+++ src/templates/hybrid-cloud-page.js
@@ -0,0 +1,292 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import Header from '../components/Header'
+import TopBar from '../components/TopBar';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero'
+import SEO from '../components/SEO'
+
+import { connect } from "react-redux";
+import LinkComponent from '../components/LinkComponent'
+
+import leftArrow from '../img/svg/arrow-left.svg'
+
+export const HybridCloudPageTemplate = ({
+  isLoggedUser,
+  title,
+  subTitle,
+  introduction,
+  useCases,
+  benefits,
+  projects,
+  organizations,
+  footer,
+}) => {
+
+  return (
+    <div>
+      <div className="wrapper project-background">
+        <TopBar />
+        <Navbar isLoggedUser={isLoggedUser} />
+        <Header title={title} subTitle={subTitle} />
+      </div>
+
+      <main className="main">
+        <div className="content">
+          {introduction &&
+            <div className="hybrid-cloud-introduction">{introduction}</div>
+          }
+          {useCases?.display &&
+            <div className="hybrid-cloud-cases">
+              <span className="title">
+                {useCases.title}
+              </span>
+              {useCases.cases.map((c, index) => {
+                return (
+                  <div className="case-wrapper" key={`useCase-${index}`}>
+                    <div className="case-image" style={{ backgroundColor: c.backgroundColor }}>
+                      <img src={
+                        (c.image?.extension === 'svg' || c.image?.extension === 'gif') && !c.image?.childImageSharp ?
+                          c.image?.publicURL
+                          :
+                          !!c.image?.childImageSharp ? c.image?.childImageSharp.fluid.src : c.image
+                      } />
+                    </div>
+                    <div className="case-text">
+                      <span dangerouslySetInnerHTML={{ __html: c.text }} />
+                      <span>{c.source}</span>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          }
+          {benefits?.display &&
+            <div className="hybrid-cloud-benefits">
+              <span className="title">
+                {benefits.title}
+              </span>
+              <div className="benefit-wrapper">
+                {benefits.items.map((benefit, index) => {
+                  return (
+                    <div key={`benefit-${index}`}>
+                      <img src={
+                        (benefit.image?.extension === 'svg' || benefit.image?.extension === 'gif') && !benefit.image?.childImageSharp ?
+                          benefit.image?.publicURL
+                          :
+                          !!benefit.image?.childImageSharp ? benefit.image?.childImageSharp.fluid.src : benefit.image
+                      } />
+                      <span>{benefit.title}</span>
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+          }
+          {projects.display &&
+            <div className="hybrid-cloud-projects">
+              <span className="title">
+                {projects.title}
+              </span>
+              {projects.list.map((project, index) => {
+                return (
+                  <div className="project-wrapper" key={`project-${index}`}>
+                    <img src={
+                      (project.image.extension === 'svg' || project.image.extension === 'gif') && !project.image.childImageSharp ?
+                        project.image.publicURL
+                        :
+                        !!project.image.childImageSharp ? project.image.childImageSharp.fluid.src : project.image
+                    } />
+                    <ul>
+                      {project.bullets.map(({ bullet }, i) => {
+                        return (
+                          <li key={`project-${index}-${i}`} dangerouslySetInnerHTML={{ __html: bullet }} />
+                        )
+                      })}
+                    </ul>
+                    {index + 1 !== projects.list.length && <hr />}
+                  </div>
+                )
+              })}
+            </div>
+          }
+          {organizations.display &&
+            <div className="hybrid-cloud-organizations">
+              <span className="title">
+                {organizations.title}
+              </span>
+              <div>
+                {organizations.list.map((organization, index) => {
+                  return (
+                    <div className="organization-wrapper" key={`organization-${index}`}>
+                      <LinkComponent href={organization.link}>
+                        <img src={
+                          (organization.image?.extension === 'svg' || organization.image?.extension === 'gif') && !organization.image?.childImageSharp ?
+                            organization.image?.publicURL
+                            :
+                            !!organization.image?.childImageSharp ? organization.image?.childImageSharp.fluid.src : organization.image
+                        } />
+                      </LinkComponent>
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+          }
+          {footer?.display &&
+            <section className="hybrid-cloud-footer">
+              <h4 className="title">{footer.title}</h4>
+              <div className="text">
+                {footer.subTitle}
+              </div>
+              <LinkComponent href={footer.button} className="footer-button">
+                <span>{footer.buttonText}</span>
+                <img src={leftArrow} alt="" />
+              </LinkComponent>
+            </section>
+          }
+        </div>
+      </main>
+    </div>
+  )
+}
+
+HybridCloudPageTemplate.propTypes = {
+  companies: PropTypes.object,
+  title: PropTypes.string,
+  subTitle: PropTypes.string,
+  footer: PropTypes.object,
+}
+
+const HybridCloudPage = ({ isLoggedUser, data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout>
+      <SEO seo={post.frontmatter.seo ? post.frontmatter.seo : null} />
+      <HybridCloudPageTemplate
+        isLoggedUser={isLoggedUser}
+        title={post.frontmatter.title}
+        subTitle={post.frontmatter.subTitle}
+        introduction={post.frontmatter.introduction}
+        useCases={post.frontmatter.useCases}
+        benefits={post.frontmatter.benefits}
+        projects={post.frontmatter.projects}
+        organizations={post.frontmatter.organizations}
+        footer={post.frontmatter.footer}
+      />
+    </Layout>
+  )
+}
+
+HybridCloudPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default connect(state => ({
+  isLoggedUser: state.loggedUserState.isLoggedUser
+}), null)(HybridCloudPage)
+
+export const hybridCloudPageQuery = graphql`
+        query HybridCloudPage($id: String!) {
+          markdownRemark(id: {eq: $id }) {
+          html
+      frontmatter {
+          seo {
+          title
+          description
+        url
+        image {
+          childImageSharp {
+          fluid(maxWidth: 640, quality: 64) {
+          ...GatsbyImageSharpFluid
+        }
+            }
+        publicURL
+          }
+        twitterUsername
+        }
+        title
+        subTitle
+        introduction
+        useCases {
+          display
+          title
+          cases {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 640, quality: 64) {
+                ...GatsbyImageSharpFluid
+                }
+              }
+              publicURL
+              extension
+            }
+            backgroundColor
+            text
+            source
+          }
+        }
+        benefits {
+          display
+          title
+          items {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 640, quality: 100) {
+                ...GatsbyImageSharpFluid
+                }
+              }
+              publicURL
+              extension
+            }
+            title
+          }
+        }
+        projects {
+          display
+          title
+          list {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 640, quality: 100) {
+                ...GatsbyImageSharpFluid
+                }
+              }
+              publicURL
+              extension
+            }
+            bullets {
+              bullet
+            }
+          }
+        }
+        organizations {
+          display
+          title
+          list {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 640, quality: 100) {
+                ...GatsbyImageSharpFluid
+                }
+              }
+              publicURL
+              extension
+            }
+            link
+          }
+        }
+        footer {
+          title
+          subTitle
+        button
+        buttonText
+        display
+        }
+      }
+    }
+  }
+`

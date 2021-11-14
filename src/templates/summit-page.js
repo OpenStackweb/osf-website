21c49ec71e0eb9a1e553a1c7df4bda1c89798e06
James Cole <james@openstack.org>
2021-11-14 16:37:08 -0600

    Fix links
    Signed-off-by: James Cole <james@openstack.org>

--- src/templates/summit-page.js
+++ src/templates/summit-page.js
@@ -93,44 +93,44 @@ export const SummitPageTemplate = ({
                   <th>Headline</th>
                   <td>$110,000</td>
                   <td>$125,000</td>
-                  <td><a href="#contact-form-horizontal">Learn More</a></td>
+                  <td><a href="#sponsorship-contact">Learn More</a></td>
                 </tr>
                 <tr>
                   <th>Premier</th>
                   <td>$75,000</td>
                   <td>$90,000</td>
-                  <td><a href="#contact-form-horizontal">Learn More</a></td>
+                  <td><a href="#sponsorship-contact">Learn More</a></td>
                 </tr>
                 <tr>
                   <th>Spotlight</th>
                   <td>$25,000</td>
                   <td>$35,000</td>
-                  <td><a href="#contact-form-horizontal">Learn More</a></td>
+                  <td><a href="#sponsorship-contact">Learn More</a></td>
                 </tr>
                 <tr>
                   <th>Exhibitor</th>
                   <td>$10,000</td>
                   <td>$15,000</td>
-                  <td><a href="#contact-form-horizontal">Learn More</a></td>
+                  <td><a href="#sponsorship-contact">Learn More</a></td>
                 </tr>
                 <tr>
                   <th>Startup</th>
                   <td>$5,000</td>
                   <td>$10,000</td>
-                  <td><a href="#contact-form-horizontal">Learn More</a></td>
+                  <td><a href="#sponsorship-contact">Learn More</a></td>
                 </tr>
                 <tr>
                   <th>Supporting</th>
                   <td>$2,500</td>
                   <td>$5,000</td>
-                  <td><a href="#contact-form-horizontal">Learn More</a></td>
+                  <td><a href="#sponsorship-contact">Learn More</a></td>
                 </tr>
               </table>
-              <span className="subhead">Interested in Sponsoring?</span>
+              <span id="sponsorship-contact" className="subhead">Interested in Sponsoring?</span>
               <span className="description">Get in touch to learn how you can help the open source users, IT decision makers and passionate developers, administrators and operators building the modern open infrastructure stack. The detailed prospectus and contract will be live on December 15, 2021.</span>
               <ContactFormHorizontal />
             </section>
-            
+
             <hr className="dividing-line" />
 
           {previousSummits && previousSummits.display &&

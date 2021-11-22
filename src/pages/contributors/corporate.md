003cfdb685418341622547c8ff09e570cc6d29fe
James Cole <james@openstack.org>
2021-11-22 16:48:05 -0600

    Add corporate contributors page
    Signed-off-by: James Cole <james@openstack.org>

---
+++ src/pages/contributors/corporate.md
@@ -0,0 +1,14 @@
+---
+templateKey: contributors-page
+seo:
+  description: >-
+    Corporate Contributors
+  image: /img/OpenInfra-icon-white.jpg
+  title: 'Corporate Contributors'
+  twitterUsername: '@OpenInfraDev'
+  url: 'https://openinfra.dev/contributors/corporate'
+title: Corporate Contributors
+subTitle:
+---
+
+The list of Corporate CLAs allowed to contribute to OpenStack projects. (Individual developers still need to fill out the Individual [Contributors License Agreement](https://wiki.openstack.org/wiki/How_To_Contribute#Contributors_License_Agreement)). If you need to update the list of employees authorized to commit code to OpenStack, please read [How To Update Corporate CLA](/cla). 
---
+++ src/templates/contributors-page.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Content, { HTMLContent } from '../components/Content'
+import Layout from '../components/Layout'
+import Header from '../components/Header'
+import TopBar from '../components/TopBar';
+import Navbar from '../components/Navbar';
+import SEO from '../components/SEO'
+
+import { connect } from "react-redux";
+
+export const ContributorsPageTemplate = ({
+  isLoggedUser,
+  title,
+  subTitle,
+  footer,
+  content,
+  contentComponent
+}) => {
+  const PageContent = contentComponent || Content
+
+  return (
+    <div>
+      <div className="wrapper project-background">
+        <TopBar />
+        <Navbar isLoggedUser={isLoggedUser} />
+        <Header title={title} subTitle={subTitle} />
+      </div>
+      <main className="main">
+        <div className="content">
+          <section className="section about-s1-main">
+            <div className="container about-s1-container">
+              <div className="columns">
+                <div className="column">
+                  <PageContent content={content} />
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+ContributorsPageTemplate.propTypes = {
+  companies: PropTypes.object,
+  title: PropTypes.string,
+  subTitle: PropTypes.string,
+}
+
+const ContributorsPage = ({ isLoggedUser, data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout>
+      <SEO seo={post.frontmatter.seo ? post.frontmatter.seo : null} />
+      <ContributorsPageTemplate
+        isLoggedUser={isLoggedUser}
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
+        subTitle={post.frontmatter.subTitle}
+        content={post.html}
+      />
+    </Layout>
+  )
+}
+
+ContributorsPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default connect(state => ({
+  isLoggedUser: state.loggedUserState.isLoggedUser
+}), null)(ContributorsPage)
+
+export const contributorsPageQuery = graphql`
+  query ContributorsPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        seo {
+          title
+          description
+          url
+          image {
+            childImageSharp {
+              fluid(maxWidth: 640, quality: 64) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+            publicURL
+          }
+          twitterUsername
+        }
+        title
+        subTitle
+      }
+    }
+  }
+`

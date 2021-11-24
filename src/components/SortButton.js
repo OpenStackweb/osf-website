b5cbfb58023c8708f8211e20645c0954d2752976
James Cole <james@openstack.org>
2021-11-23 20:04:39 -0600

    Add arrow toggle
    Signed-off-by: James Cole <james@openstack.org>

--- src/components/SortButton.js
+++ src/components/SortButton.js
@@ -1,17 +1,39 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 class SortButton extends React.Component {
   
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.changeIcon = this.changeIcon.bind(this);
+    this.state = {
+      direction: true,
+      id: "id"
+    };
+  }
+
+  changeIcon() {
+    if (this.id.className === "fa fa-chevron-up sort-icon") {
+      alert("it is true");
+    } 
     
+    else if (this.className !== "fa fa-chevron-up sort-icon") {
+      alert("it is false");
+    }
   }
   
   handleClick(event) {
+    event.preventDefault();
     const id = event.target.id;
-    document.getElementById(id).style = "transform: rotate(180deg)";
-  
+    this.setState(prevState => ({ direction: !prevState.direction }));
+    this.setState({id: id});
+
+    if (document.getElementById(id).className === "fa fa-chevron-down sort-icon") {
+      (document.getElementById(id).className = "fa fa-chevron-up sort-icon")
+    } else if (document.getElementById(id).className === "fa fa-chevron-up sort-icon") {
+      (document.getElementById(id).className = "fa fa-chevron-down sort-icon")
+    }
+
   }
 
   render() {

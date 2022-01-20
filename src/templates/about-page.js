c58f0b60ebab476d8ca50ad829387ef36a7ed7c0
Wes Wilson <wes@iamweswilson.com>
2022-01-20 15:11:30 -0800

    about template, logo replacing title

---
+++ src/templates/about-page.js
@@ -0,0 +1,114 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Content, { HTMLContent } from '../components/Content'
+import Layout from '../components/Layout'
+import Header from '../components/Header'
+import TopBar from '../components/TopBar';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero'
+import SEO from '../components/SEO'
+
+import { connect } from "react-redux";
+
+export const AboutPageTemplate = ({
+  isLoggedUser,
+  footer,
+  content,
+  contentComponent
+}) => {
+  const PageContent = contentComponent || Content
+
+  return (
+    <div>
+      <div className="wrapper project-background">
+        <TopBar />
+        <Navbar isLoggedUser={isLoggedUser} />
+      </div>
+
+      <main className="main">
+        <div className="content">
+          <section className="section about-s1-main no-top-padding">
+            <div className="container about-s1-container">
+              <div className="columns">
+                <div className="column">
+                  <img class="about-full-logo" src="/img/OpenInfrastructureFoundation-logo-RGB-horiz2.png" />
+                  <PageContent content={content} />
+                </div>
+              </div>
+            </div>
+          </section>
+          {footer &&
+            <Hero content={footer} />
+          }
+        </div>
+      </main>
+    </div>
+  )
+}
+
+AboutPageTemplate.propTypes = {
+  companies: PropTypes.object,
+  title: PropTypes.string,
+  subTitle: PropTypes.string,
+  footer: PropTypes.object,
+}
+
+const AboutPage = ({ isLoggedUser, data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout>
+      <SEO seo={post.frontmatter.seo ? post.frontmatter.seo : null} />
+      <AboutPageTemplate
+        isLoggedUser={isLoggedUser}
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
+        subTitle={post.frontmatter.subTitle}
+        footer={post.frontmatter.footer}
+        content={post.html}
+      />
+    </Layout>
+  )
+}
+
+AboutPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default connect(state => ({
+  isLoggedUser: state.loggedUserState.isLoggedUser
+}), null)(AboutPage)
+
+export const aboutPageQuery = graphql`
+  query AboutPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        seo {
+          title
+          description
+          url
+          image {
+            childImageSharp {
+              fluid(maxWidth: 640, quality: 64) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+            publicURL
+          }
+          twitterUsername
+        }
+        title
+        subTitle
+        footer {
+          title
+          subTitle
+          button
+          buttonText
+          display
+        }
+      }
+    }
+  }
+`

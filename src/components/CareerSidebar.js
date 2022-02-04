9491e043bbb0a0d54a0f14046a5af9c4e11180f0
James Cole <james@openstack.org>
2022-02-04 13:26:52 -0600

    Replace arrow
    Signed-off-by: James Cole <james@openstack.org>

--- src/components/CareerSidebar.js
+++ src/components/CareerSidebar.js
@@ -9,7 +9,14 @@ const CareerSidebar = class extends React.Component {
     return (
       <aside class="sidebar">
         <p class="menu-title">
-          <a href="/careers/" className="menu-back">All OpenInfra Roles</a>
+          <a href="/careers/" className="menu-back">
+          <div className="title-arrow">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+            </svg>
+          </div>
+          <p>All OpenInfra Roles</p>
+          </a>
         </p>
         <ul class="menu-list">
           <li>

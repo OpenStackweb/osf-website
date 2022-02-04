233b1220a4cc3cea92e4c5f8543b74dc32f8c598
James Cole <james@openstack.org>
2022-02-04 12:58:59 -0600

    Add more buttons
    Signed-off-by: James Cole <james@openstack.org>

--- src/templates/careers-page.js
+++ src/templates/careers-page.js
@@ -47,7 +47,7 @@ export const CareersPageTemplate = ({
                   <h3 className="fix-h3">{row1.title1}</h3>
                   <p>{row1.text1}</p>
                   <p>
-                  <LinkComponent className="button-cta" href="#openings">See Current Openings<img src={leftArrow} alt="Left Arrow" /></LinkComponent> 
+                    <LinkComponent style={{marginTop: "20px"}} className="button-cta" href="#openings">See Current Openings<img src={leftArrow} alt="Left Arrow" /></LinkComponent> 
                   </p>
                 </div>
               </div>
@@ -61,7 +61,7 @@ export const CareersPageTemplate = ({
                         It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
                       </p>
                       <p>
-                        <a href="/careers/developer-advocate" className="career-btn">Learn More</a>
+                        <LinkComponent style={{marginTop: "20px"}} className="button-cta" href="/careers/developer-advocate">Learn More<img src={leftArrow} alt="Left Arrow" /></LinkComponent> 
                       </p>
                   </div>
                     <div className="career-single">
@@ -70,7 +70,7 @@ export const CareersPageTemplate = ({
                         It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
                       </p>
                       <p>
-                        <a href="/careers/accounting-operations-intern" className="career-btn">Learn More</a>
+                        <LinkComponent style={{marginTop: "20px"}} className="button-cta" href="/careers/accounting-operations-intern">Learn More<img src={leftArrow} alt="Left Arrow" /></LinkComponent> 
                       </p>
                     </div>
                     <hr />

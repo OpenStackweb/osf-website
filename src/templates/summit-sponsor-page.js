ad36c44e62cc3289c8d37edc32bf77f4d4eda9a2
Wes Wilson <wes@iamweswilson.com>
2022-02-16 09:40:32 -0800

    prospectus - Sold out Lanyard

--- src/templates/summit-sponsor-page.js
+++ src/templates/summit-sponsor-page.js
@@ -104,8 +104,8 @@ export const SummitSponsorPageTemplate = ({
           <div className="title">How to Sponsor</div>
             <div className="step-single">
               <h5>Step 1: Prospectus</h5>
-              <p><a href="/files/OpenInfraSummitBerlin2022-prospectusV2.pdf">Review the Prospectus</a> and decide which sponsorship levels and add-ons you are interested in.</p>
-              <a href="/files/OpenInfraSummitBerlin2022-prospectusV2.pdf" className="button-cta outline">Review the Prospectus</a>
+              <p><a href="/files/OpenInfra-Berlin-SUMMIT-2022-prospectus_021622.pdf">Review the Prospectus</a> and decide which sponsorship levels and add-ons you are interested in.</p>
+              <a href="/files/OpenInfra-Berlin-SUMMIT-2022-prospectus_021622.pdf" className="button-cta outline">Review the Prospectus</a>
             </div>
             <div className="step-single">
               <h5>Step 2: Master Sponsor Agreement (New Sponsors Only)</h5>

5c909468510d662f88314473e17cd0fd50cec27e
Tomás Castillo <tcastilloboireau@gmail.com>
2022-03-22 10:49:27 -0300

    Base newsletter page
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

---
+++ src/pages/newsletter/index.md
@@ -0,0 +1,34 @@
+---
+templateKey: newsletter-page
+seo:
+  title: OpenInfra Live
+  description: Interactive Conversations around All Things Open Infrastructure
+  url: https://openinfra.dev/newsletter/
+  image: /img/oificon-social.png
+  twitterUsername: "@OpenInfraDev"
+header:
+  display: true
+  upperTitle: STAY UP TO DATE
+  title: GET THE LATEST IN OPENINFRA DELIVERED TO YOUR INBOX
+  description: >-
+    "With over 110,000 members in 187 countries, the OpenInfra Foundation provides a 
+    home for open infrastructure projects, including OpenStack, as well as open source projects 
+    Airship (infrastructure lifecycle management), Kata Containers (secure containers), OpenInfra 
+    Labs (connecting open source projects to production), StarlingX (edge cloud platform) and Zuul 
+    (CI/CD platform). The monthly <a href='https://superuser.openstack.org/articles/tag/newsletter/'
+    target='_blank' rel='noopener noreferrer'>OpenInfra community newsletter</a> brings the latest 
+    developments and activities across OpenInfra projects, events, and users from the OpenInfra 
+    community to your inbox."
+signup:
+  title: Sign up now!
+  description: >-
+    "Sign up to the OpenInfra newsletter to stay up to date on the OpenInfra projects, get 
+    involved in the community, and be the first to know about the latest happenings in the 
+    world of open infrastructure like industry updates, cloud economics, and more! Our goal 
+    is to help educate and engage the OpenInfra community, and we welcome you to participate!"
+footer:
+  title: "Already signed up for our mailing list? Now explore our past newsletters!"
+  buttonText: READ NOW
+  button: https://superuser.openstack.org/articles/tag/newsletter/
+  display: true
+---
---
+++ src/templates/newsletter-page.js
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import { HTMLContent } from '../components/Content'
+import Layout from '../components/Layout'
+import TopBar from '../components/TopBar';
+import Navbar from '../components/Navbar';
+import SEO from '../components/SEO'
+import leftArrow from '../img/svg/arrow-left.svg'
+
+import { connect } from "react-redux";
+
+import LinkComponent from '../components/LinkComponent'
+
+export const NewsletterPageTemplate = ({
+  isLoggedUser,
+  contentComponent,
+  header,
+  signup,
+  footer,
+}) => {
+
+  return (
+    <div>
+      <div className="wrapper project-background">
+        <TopBar />
+        <Navbar isLoggedUser={isLoggedUser} />
+      </div>
+
+      <main className="main">
+        <div className="content">
+          {header && header.display &&
+            <section className="newsletter-header">
+              <div className="header-left">
+                <span className="upper-title">
+                  {header.upperTitle}
+                </span>
+                <span className="title">
+                  {header.title}
+                </span>
+                <span className="description" dangerouslySetInnerHTML={{ __html: header.description }}></span>
+              </div>
+              <div className="header-right">
+                <div className="hero-video">
+                  <iframe width="560" height="315" src="https://www.youtube.com/embed/gitMjvPnUG0" frameBorder={"0"} allowFullScreen></iframe>
+                </div>
+                <div className="picture">
+                  {/* <img src={!!header.image.childImageSharp ? header.image.childImageSharp.fluid.src : header.image} /> */}
+                </div>
+              </div>
+            </section>
+          }
+
+          <section className="newsletter-signup">
+            <span className="title">
+              {signup.title}
+            </span>
+            <span className="description" dangerouslySetInnerHTML={{ __html: signup.description }}></span>
+          </section>
+          <div className='newsletter-iframe'>
+            <iframe src="https://app.e2ma.net/app2/audience/signup/1958430/1771360/" scrolling='no' width={'100%'} height={'100%'} frameborder="0"></iframe>
+          </div>
+
+          <div className='newsletter-line'>
+            <div /><div /><div /><div />
+          </div>
+          <section className="newsletter-footer">
+            <span>{footer.title}</span>
+            <LinkComponent className="button-cta" href={footer.button}>{footer.buttonText}<img src={leftArrow} alt="" /></LinkComponent>
+          </section>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+const NewsletterPage = ({ data, isLoggedUser }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout>
+      <SEO seo={post.frontmatter.seo ? post.frontmatter.seo : null} />
+      <NewsletterPageTemplate
+        contentComponent={HTMLContent}
+        header={post.frontmatter.header}
+        signup={post.frontmatter.signup}
+        footer={post.frontmatter.footer}
+        isLoggedUser={isLoggedUser}
+      />
+    </Layout>
+  )
+}
+
+NewsletterPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default connect(state => ({
+  isLoggedUser: state.loggedUserState.isLoggedUser
+}), null)(NewsletterPage)
+
+
+export const NewsletterPageQuery = graphql`
+  query NewsletterPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        seo {
+          title
+          description
+          url
+          image {
+            childImageSharp {
+              fluid(maxWidth: 640, quality: 64) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+            publicURL
+          }
+          twitterUsername
+        }
+        header {
+          display
+          title
+          upperTitle
+          description
+        }
+        signup {
+          title
+          description
+        }
+        footer {
+          title
+          button
+          buttonText
+          display
+        }
+      }
+    }
+  }
+`

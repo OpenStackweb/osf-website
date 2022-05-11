d0d9633e88f95c2627259b9bccdd61a57d06b95d
smarcet <smarcet@gmail.com>
2022-05-11 20:05:55 -0300

    Added function validation
    Signed-off-by: smarcet <smarcet@gmail.com>

--- netlify/functions/sf-contact-form-post.js
+++ netlify/functions/sf-contact-form-post.js
@@ -1,17 +1,41 @@
 const fetch = require("node-fetch");
 const { GATSBY_SF_OID } = process.env;
+const emailValidator = require("email-validator");
 
 exports.handler = async function (event, context) {
-    // todo: check params presence
 
-    let body = event.body+`&oid=${GATSBY_SF_OID}`;
+    const requiredParams = ['email', 'first_name', 'last_name', 'company', 'title']
+
+    let formData = event.body;
+    const decodedData = decodeURIComponent(formData);
+    const params = new URLSearchParams(decodedData);
+    // add sale force account id
+    params.append('oid', GATSBY_SF_OID);
+
+    for(let p of requiredParams) {
+        if (!params.has(p)) {
+            return {
+                statusCode: 412,
+                body: `${p} param is required.`,
+            }
+        }
+    }
+
+    // validate email
+    const email = params.get('email');
+    if(!emailValidator.validate(email)){
+        return {
+            statusCode: 412,
+            body: 'email param is invalid',
+        }
+    }
 
     return fetch('https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8', {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
         },
         method: "POST",
-        body: body,
+        body: params.toString(),
     })
         .then(() => {
             return {

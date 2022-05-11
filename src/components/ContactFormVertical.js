2a8df0b90a9b02fd28a3cba1c49146ea69162184
smarcet <smarcet@gmail.com>
2022-05-11 19:10:14 -0300

    Removed debugger statements refactoring
    Signed-off-by: smarcet <smarcet@gmail.com>

--- src/components/ContactFormVertical.js
+++ src/components/ContactFormVertical.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Swal from "sweetalert2";
 
 const ContactForm = () => {
     const [inputs, setInputs] = useState({});
@@ -48,50 +49,69 @@ const ContactForm = () => {
                 method: "POST",
                 body: formBody,
             }).then(() => {
-                debugger;
                 setSuccess(true);
             }).catch(e => {
-                debugger;
+                setSuccess(false);
                 console.log(e);
-        });
+                Swal.fire("Error", 'Oops! Something went wrong.', "warning");
+            });
         evt.preventDefault();
         return false
     }
 
     return(
         <form className="contact-form" onSubmit={handleSubmit}>
-            <div id="form-fields">
-            <div className="form-wrapper is-vertical">
-                <div className="field-column is-full-width">
-                    <div className="field-row ">
-                        <label for="first_name"></label><input id="first_name" className="contact-field lt-field" maxlength="40" value={inputs.first_name || ""} onChange={handleChange} name="first_name" type="text" placeholder="First Name" required/>
-                        <label for="last_name"></label><input id="last_name" className="contact-field rt-field" maxlength="80" value={inputs.last_name || ""} onChange={handleChange} name="last_name"  type="text" placeholder="Last Name" required/>
+            {!success &&
+                <div id="form-fields">
+                    <div className="form-wrapper is-vertical">
+                        <div className="field-column is-full-width">
+                            <div className="field-row ">
+                                <label for="first_name"></label><input id="first_name" className="contact-field lt-field"
+                                                                       maxlength="40" value={inputs.first_name || ""}
+                                                                       onChange={handleChange} name="first_name" type="text"
+                                                                       placeholder="First Name" required/>
+                                <label for="last_name"></label><input id="last_name" className="contact-field rt-field"
+                                                                      maxlength="80" value={inputs.last_name || ""}
+                                                                      onChange={handleChange} name="last_name" type="text"
+                                                                      placeholder="Last Name" required/>
+                            </div>
+                            <div className="field-row ">
+                                <label for="company"></label><input id="company" className="contact-field lt-field"
+                                                                    maxlength="40" name="company"
+                                                                    value={inputs.company || ""} onChange={handleChange}
+                                                                    type="text" placeholder="Company" required/>
+
+                                <label for="title"></label><input id="title" className="contact-field rt-field"
+                                                                  maxlength="40" name="title" value={inputs.title || ""}
+                                                                  onChange={handleChange} type="text" placeholder="Title"
+                                                                  required/>
+                            </div>
+                            <div className="field-row ">
+                                <label for="email"></label><input id="email" className="contact-field ct-field"
+                                                                  maxlength="80" name="email" value={inputs.email || ""}
+                                                                  onChange={handleChange} type="email" placeholder="Email"
+                                                                  required/>
+                            </div>
                         </div>
-                    <div className="field-row ">
-                        <label for="company"></label><input id="company" className="contact-field lt-field" maxlength="40" name="company" value={inputs.company || ""} onChange={handleChange} type="text" placeholder="Company" required/>
-                        
-                        <label for="title"></label><input id="title" className="contact-field rt-field" maxlength="40" name="title" value={inputs.title || ""} onChange={handleChange} type="text" placeholder="Title" required/>
+                        <div className="field-column is-full-width">
+                            <textarea id="00N6f00000FmlhK" className="message-field" name="00N6f00000FmlhK" type="text"
+                                      placeholder="How can we help?" wrap="soft" required
+                                      value={inputs['00N6f00000FmlhK'] || ""} onChange={handleChange}></textarea>
+                            <button className="contact-submit" type="submit" name="submit">SUBMIT</button>
+                        </div>
                     </div>
-                    <div className="field-row ">
-                        <label for="email"></label><input id="email" className="contact-field ct-field" maxlength="80" name="email" value={inputs.email || ""} onChange={handleChange} type="email" placeholder="Email" required/>
-                    </div>
                 </div>
-                <div className="field-column is-full-width">
-                    <textarea id="00N6f00000FmlhK" className="message-field" name="00N6f00000FmlhK" type="text" placeholder="How can we help?" wrap="soft" required value={inputs['00N6f00000FmlhK'] || ""} onChange={handleChange}></textarea>
-                    <button className="contact-submit" type="submit" name="submit">SUBMIT</button>
-                </div>
-                </div>
-            </div>
+            }
             {success &&
-            <div id="confirmation-message">
-                <div className="confirmation-text">Thank you for contacting the Open Infrastructure Foundation. Someone
-                    from the Foundation will follow up with you as soon as possible. If you’d like to set up a meeting
-                    directly with our business development team, go ahead and <a className="form-links"
-                                                                                 href="https://calendly.com/jimmy-mcarthur"
-                                                                                 target="_blank">grab some time on our
-                        calendar</a>.
+                <div id="confirmation-message">
+                    <div className="confirmation-text">Thank you for contacting the Open Infrastructure Foundation. Someone
+                        from the Foundation will follow up with you as soon as possible. If you’d like to set up a meeting
+                        directly with our business development team, go ahead and <a className="form-links"
+                                                                                     href="https://calendly.com/jimmy-mcarthur"
+                                                                                     target="_blank">grab some time on our
+                            calendar</a>.
+                    </div>
                 </div>
-            </div>
             }
         </form>
     )

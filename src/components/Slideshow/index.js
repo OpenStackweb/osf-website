c26ca8786331a32a99ea7f76da95d473d0ed1178
smarcet <smarcet@gmail.com>
2022-05-31 16:48:22 -0300

    Slide Show Fix
    * calculated container width dynamically
    * fixed css
    * refactoring
    
    Signed-off-by: smarcet <smarcet@gmail.com>

--- src/components/Slideshow/index.js
+++ src/components/Slideshow/index.js
@@ -2,13 +2,19 @@ import React from 'react'
 import './styles.scss';
 
 const Slideshow = ({children}) => {
+    const ImageWidth = 200;
+
+    const getContainerWidth = (images) => {
+        return `${images.length * ImageWidth}px`;
+    }
+
     return (
-      <div className="slideshow">
-        <div className="overlay" />
-        <div className="images">
-          {children}
+        <div className="slideshow">
+            <div className="overlay"/>
+            <div className="images" style={{width: getContainerWidth(children)}}>
+                {children}
+            </div>
         </div>
-      </div>
     )
 }
 

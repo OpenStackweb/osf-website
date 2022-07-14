1762a61004be8b94c846cdd466e8144042cf29bf
Tomás Castillo <tcastilloboireau@gmail.com>
2022-07-14 10:25:39 -0300

    Update navigation widget version
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -35,7 +35,10 @@ const TemplateWrapper = ({ children, style, lastBuild, syncData }) => {
                 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
                 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
             </Helmet>
-            <NavigationWidget projects={sponsoredProjects} currentProject={currentProject} />
+            <NavigationWidget 
+                projects={sponsoredProjects}
+                currentProject={currentProject}
+                navbarTitle={'View OpenInfra Projects'} />
             <div>{children}</div>
             <Footer />
         </div>

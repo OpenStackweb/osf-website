c654e886d989d63bdd424988092f5db0b99b32b7
Josh Lohse <yoshwaaa@gmail.com>
2022-07-26 13:54:37 -0400

    Updated to include visa letter subnav link and faq text

--- src/components/PTGSubNav.js
+++ src/components/PTGSubNav.js
@@ -4,6 +4,7 @@ import LinkComponent from './LinkComponent';
 function PTGSubNav(props) {
 
   const [isOpen, setIsOpen] = useState(false);
+  const [supportDropdown, setSupportDropdown] = useState(false);
 
   useEffect(() => {
     let active = document.getElementById(props.active);
@@ -34,9 +35,26 @@ function PTGSubNav(props) {
 
         <ul id="links-list" className="links-list">
           <li><LinkComponent id="ptg" href="/ptg" onClick={() => shouldCloseMenu("ptg")} className="link">About</LinkComponent></li>
-          <li><LinkComponent id="ptg-faq" href="/ptg/faq" onClick={() => shouldCloseMenu("ptg-faq")} className="link">FAQ</LinkComponent></li>
-          <li><LinkComponent id="ptg-safety" href="/ptg/safety" onClick={() => shouldCloseMenu("ptg-safety")} className="link">Health & Safety</LinkComponent></li>
           <li><a id="ptg-sponsor" href="https://openinfra.dev/files/PTG-Columbus2022-Prospectus-062722.pdf" onClick={() => shouldCloseMenu("ptg-sponsor")} className="link">Sponsor Info</a></li>
+          <li onMouseEnter={() => setSupportDropdown(true)} onMouseLeave={() => setSupportDropdown(false)} style={{ marginBottom: -33, paddingBottom: 30 }}>
+            <LinkComponent id="ptg-faq" href="/ptg/faq" className="link" style={{ padding: 10, width: '110%', display: 'inline-flex' }}>
+              Support
+              <i style={{ marginLeft: "auto" }} className={`fa fa-chevron-down`} />
+            </LinkComponent>
+            {supportDropdown &&
+              <div className='dropdown-options'>
+                <LinkComponent id="ptg-faq" href="/ptg/faq" className="link dropdown">
+                  FAQ
+                </LinkComponent>
+                <LinkComponent id="ptg-faq" href="/ptg/safety" className="link dropdown">
+                  <span>Health & Safety</span>
+                </LinkComponent>
+                <LinkComponent id="ptg-visa-form" href="https://openinfrafoundation.formstack.com/forms/visa_columbus2022" className="link dropdown">
+                  <span>Visa Letter Request</span>
+                </LinkComponent>
+              </div>
+            }
+          </li>
           <li><LinkComponent id="ptg-hotel" href="https://www.hyatt.com/en-US/group-booking/CMHRC/G-L0RT" onClick={() => shouldCloseMenu("ptg-hotel")} className="link">Book Your Hotel</LinkComponent></li>
           <li><LinkComponent id="registration" href="https://openinfra-ptg.eventbrite.com" className="link registration">Register</LinkComponent></li>
         </ul>
@@ -52,8 +70,25 @@ function PTGSubNav(props) {
         <div className="subnav-dropdown">
           <ul id="links-list-mobile" className="links-list">
           <li><LinkComponent id="ptg-mobile" href="/ptg" onClick={() => shouldCloseMenu("ptg")} className="link">About</LinkComponent></li>
-          <li><LinkComponent id="ptg-faq-mobile" href="/ptg/faq" onClick={() => shouldCloseMenu("ptg-faq")} className="link">FAQ</LinkComponent></li>
-          <li><LinkComponent id="ptg-safety-mobile" href="/ptg/safety" onClick={() => shouldCloseMenu("ptg-safety")} className="link">Health & Safety</LinkComponent></li>
+          <li onMouseEnter={() => setSupportDropdown(true)} onMouseLeave={() => setSupportDropdown(false)} className="link-dropdown">
+              <LinkComponent id="ptg-faq-mobile" href="#" className="link link-dropdown-parent" >
+                Support
+                <i style={{ marginLeft: 5 }} className={`fa fa-chevron-down`} />
+              </LinkComponent>
+              {supportDropdown &&
+                <>
+                  <LinkComponent id="ptg-faq-mobile" href="/ptg/faq" onClick={() => shouldCloseMenu("ptg")} className="link-dropdown-option" >
+                    FAQs
+                  </LinkComponent>
+                  <LinkComponent id="ptg-safety-mobile" href="/ptg/safety" onClick={() => shouldCloseMenu("ptg")} className="link-dropdown-option">
+                    <span>Health & Safety</span>
+                  </LinkComponent>
+                  <LinkComponent id="ptg-visa-form-mobile" href="https://openinfrafoundation.formstack.com/forms/visa_columbus2022" onClick={() => shouldCloseMenu("ptg-visa-form-mobile")} className="link-dropdown-option">
+                    <span>Visa Letter Request</span>
+                  </LinkComponent>
+                </>
+              }
+            </li>
           <li><a id="ptg-sponsor=mobile" href="https://openinfra.dev/files/PTG-Columbus2022-Prospectus-062722.pdf" onClick={() => shouldCloseMenu("ptg-sponsor")} className="link">Sponsor Info</a></li>
           <li><LinkComponent id="ptg-hotel-mobile" href="https://www.hyatt.com/en-US/group-booking/CMHRC/G-L0RT" onClick={() => shouldCloseMenu("ptg-hotel")} className="link">Book Your Hotel</LinkComponent></li>
           <li><LinkComponent id="registration-mobile" href="https://openinfra-ptg.eventbrite.com" className="link registration">Register</LinkComponent></li>

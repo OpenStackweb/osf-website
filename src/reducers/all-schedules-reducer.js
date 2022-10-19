c8a4fa569fc6cc4ff0fa0dd69d7bf67cff4e7b46
Tomás Castillo <tcastilloboireau@gmail.com>
2022-10-19 19:28:30 -0300

    Temporary disable events
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/reducers/all-schedules-reducer.js
+++ src/reducers/all-schedules-reducer.js
@@ -1,6 +1,6 @@
 import scheduleReducer from './schedule-reducer';
 import summitData from '../content/summit.json';
-import eventsData from '../content/events.json';
+// import eventsData from '../content/events.json';
 import {filterEventsByTags} from '../utils/schedule';
 import {LOGOUT_USER} from "openstack-uicore-foundation/lib/actions";
 import {UPDATE_FILTER, UPDATE_FILTERS, CHANGE_VIEW, CHANGE_TIMEZONE, RELOAD_SCHED_DATA , RELOAD_USER_PROFILE} from '../actions/schedule-actions'
@@ -11,7 +11,7 @@ import {ADD_TO_SCHEDULE, REMOVE_FROM_SCHEDULE, GET_USER_PROFILE} from "../action
 const scheduleEvents = filterEventsByTags(eventsData);
 
 const DEFAULT_STATE = {
-    allEvents: eventsData,
+    allEvents: [],
     allScheduleEvents: scheduleEvents,
     schedules: []
 };

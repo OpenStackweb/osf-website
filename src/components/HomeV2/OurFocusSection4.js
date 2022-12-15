d756148d775ec3e6830b527d3452a61ef117c213
Josh Lohse <yoshwaaa@gmail.com>
2022-12-15 15:23:57 -0500

    Fixed timing issue

--- src/components/HomeV2/OurFocusSection4.js
+++ src/components/HomeV2/OurFocusSection4.js
@@ -20,7 +20,7 @@ const OurFocusSection4 = () => {
             pin: true,
             scrub: 0.1,
             // toggleClass: 'enable',
-            end: "+=700",
+            end: "+=500",
             onEnter: () => {
                 document.querySelector("#try-panel-1").classList.add("enable");
                 document.querySelector("#scroller-container").classList.add("enable-slide-1"); document.querySelector("#scroller-container").classList.remove("enable-slide-2"); document.querySelector("#scroller-container").classList.remove("enable-slide-3");
@@ -41,7 +41,7 @@ const OurFocusSection4 = () => {
             pin: true,
             scrub: 0.1,
             // toggleClass: 'enable',
-            end: "+=700",
+            end: "+=500",
             onEnter: () => {
                 document.querySelector("#try-panel-2").classList.add("enable");
                 document.querySelector("#scroller-container").classList.add("enable-slide-2"); document.querySelector("#scroller-container").classList.remove("enable-slide-1"); document.querySelector("#scroller-container").classList.remove("enable-slide-3");
@@ -62,7 +62,7 @@ const OurFocusSection4 = () => {
             pin: true,
             scrub: 0.1,
             // toggleClass: 'enable',
-            end: "+=700",
+            end: "+=500",
             onEnter: () => {
                 document.querySelector("#try-panel-3").classList.add("enable");
                 document.querySelector("#scroller-container").classList.add("enable-slide-3"); document.querySelector("#scroller-container").classList.remove("enable-slide-1"); document.querySelector("#scroller-container").classList.remove("enable-slide-2");

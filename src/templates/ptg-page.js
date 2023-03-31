8b5de614c8a3582f961e7169e08a77d8f7ff99d6
Josh Lohse <yoshwaaa@gmail.com>
2023-03-31 13:32:03 -0400

    Updated with spacing fixes

--- src/templates/ptg-page.js
+++ src/templates/ptg-page.js
@@ -56,7 +56,7 @@ export const PTGPageTemplate = ({
                   <img src={(header.location.icon.extension === 'svg' || header.location.icon.extension === 'gif') && !header.location.icon.childImageSharp ?
                     header.location.icon.publicURL
                     :
-                    !!header.location.icon.childImageSharp ? header.location.icon.childImageSharp.fluid.src : header.location.icon} /> {header.date.location}
+                    !!header.location.icon.childImageSharp ? header.location.icon.childImageSharp.fluid.src : header.location.icon} /> Vancouver Convention Centre
                 </span>
                 <div className="buttons">
                     {header.buttons.map((button, index) => {

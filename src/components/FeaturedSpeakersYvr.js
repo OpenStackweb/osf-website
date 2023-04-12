32c98bcc89d33b156d2dc00ebea71db7fdf61e89
Josh Lohse <yoshwaaa@gmail.com>
2023-04-12 14:22:16 -0400

    Added first round of featured speakers for vancouver summit

--- src/components/FeaturedSpeakersYvr.js
+++ src/components/FeaturedSpeakersYvr.js
@@ -9,74 +9,92 @@ const FeaturedSpeakersYvr = ({ featured_speakers }) => {
             <div className='featured-speakers-list'>
             <div>
                 <div className="featured-speaker-image">
-                    <img src="https://object-storage.public.mtl1.vexxhost.net/swift/v1/6e4619c416ff4bd19e1c087f27a43eea/www-assets-prod/profile-images/JonathanBryce-Headshot.JPG" />
+                    <img src="https://object-storage.public.mtl1.vexxhost.net/swift/v1/6e4619c416ff4bd19e1c087f27a43eea/www-assets-prod/profile-images/ThierryCarrez-Headshot1.jpg" />
                 </div>
                 <div className="featured-speaker-info">
-                    <div className="featured-speaker-name">Jonathan Bryce</div>
+                    <div className="featured-speaker-name">Thierry Carrez</div>
                     <div className="featured-speaker-company">OpenInfra Foundation</div>
                 </div>
             </div>
             <div>
                 <div className="featured-speaker-image">
-                    <img src="https://object-storage.public.mtl1.vexxhost.net/swift/v1/6e4619c416ff4bd19e1c087f27a43eea/www-assets-prod/profile-images/JonathanBryce-Headshot.JPG" />
+                    <img src="/img/summit/vancouver-2023/paul_milller_small.jpg" />
                 </div>
                 <div className="featured-speaker-info">
-                    <div className="featured-speaker-name">Jonathan Bryce</div>
-                    <div className="featured-speaker-company">OpenInfra Foundation</div>
+                    <div className="featured-speaker-name">Paul Miller</div>
+                    <div className="featured-speaker-company">Wind River</div>
                 </div>
             </div>
             <div>
                 <div className="featured-speaker-image">
-                    <img src="https://object-storage.public.mtl1.vexxhost.net/swift/v1/6e4619c416ff4bd19e1c087f27a43eea/www-assets-prod/profile-images/JonathanBryce-Headshot.JPG" />
+                    <img src="/img/summit/vancouver-2023/EBocchi.jpg" />
                 </div>
                 <div className="featured-speaker-info">
-                    <div className="featured-speaker-name">Jonathan Bryce</div>
-                    <div className="featured-speaker-company">OpenInfra Foundation</div>
+                    <div className="featured-speaker-name">Enrico Bocchi</div>
+                    <div className="featured-speaker-company">CERN</div>
                 </div>
             </div>
             <div>
                 <div className="featured-speaker-image">
-                    <img src="https://object-storage.public.mtl1.vexxhost.net/swift/v1/6e4619c416ff4bd19e1c087f27a43eea/www-assets-prod/profile-images/JonathanBryce-Headshot.JPG" />
+                    <img src="/img/summit/vancouver-2023/Nisha Brahmankar.jpeg" />
                 </div>
                 <div className="featured-speaker-info">
-                    <div className="featured-speaker-name">Jonathan Bryce</div>
-                    <div className="featured-speaker-company">OpenInfra Foundation</div>
+                    <div className="featured-speaker-name">Nisha Brahmankar</div>
+                    <div className="featured-speaker-company">LINE Corporation</div>
                 </div>
             </div>
             <div>
                 <div className="featured-speaker-image">
-                    <img src="https://object-storage.public.mtl1.vexxhost.net/swift/v1/6e4619c416ff4bd19e1c087f27a43eea/www-assets-prod/profile-images/JonathanBryce-Headshot.JPG" />
+                    <img src="/img/summit/vancouver-2023/JingGENG.jpg" />
                 </div>
                 <div className="featured-speaker-info">
-                    <div className="featured-speaker-name">Jonathan Bryce</div>
-                    <div className="featured-speaker-company">OpenInfra Foundation</div>
+                    <div className="featured-speaker-name">Jing Geng</div>
+                    <div className="featured-speaker-company">Bloomberg</div>
                 </div>
             </div>
             <div>
                 <div className="featured-speaker-image">
-                    <img src="https://object-storage.public.mtl1.vexxhost.net/swift/v1/6e4619c416ff4bd19e1c087f27a43eea/www-assets-prod/profile-images/JonathanBryce-Headshot.JPG" />
+                    <img src="/img/summit/vancouver-2023/Howard-Abrams.jpg" />
                 </div>
                 <div className="featured-speaker-info">
-                    <div className="featured-speaker-name">Jonathan Bryce</div>
-                    <div className="featured-speaker-company">OpenInfra Foundation</div>
+                    <div className="featured-speaker-name">Howard Abrams</div>
+                    <div className="featured-speaker-company">Workday</div>
                 </div>
             </div>
             <div>
                 <div className="featured-speaker-image">
-                    <img src="https://object-storage.public.mtl1.vexxhost.net/swift/v1/6e4619c416ff4bd19e1c087f27a43eea/www-assets-prod/profile-images/JonathanBryce-Headshot.JPG" />
+                    <img src="/img/summit/vancouver-2023/Julia-Kreger.png" />
                 </div>
                 <div className="featured-speaker-info">
-                    <div className="featured-speaker-name">Jonathan Bryce</div>
-                    <div className="featured-speaker-company">OpenInfra Foundation</div>
+                    <div className="featured-speaker-name">Julia Kreger</div>
+                    <div className="featured-speaker-company">Red Hat</div>
                 </div>
             </div>
             <div>
                 <div className="featured-speaker-image">
-                    <img src="https://object-storage.public.mtl1.vexxhost.net/swift/v1/6e4619c416ff4bd19e1c087f27a43eea/www-assets-prod/profile-images/JonathanBryce-Headshot.JPG" />
+                    <img src="/img/summit/vancouver-2023/Tytus-Kurek.jpeg" />
                 </div>
                 <div className="featured-speaker-info">
-                    <div className="featured-speaker-name">Jonathan Bryce</div>
-                    <div className="featured-speaker-company">OpenInfra Foundation</div>
+                    <div className="featured-speaker-name">Tytus Kurek</div>
+                    <div className="featured-speaker-company">Canonical</div>
+                </div>
+            </div>
+            <div>
+                <div className="featured-speaker-image">
+                    <img src="/img/summit/vancouver-2023/johannes-foufas.jpg" />
+                </div>
+                <div className="featured-speaker-info">
+                    <div className="featured-speaker-name">Johannes Foufas</div>
+                    <div className="featured-speaker-company">Volvo Car Corporation</div>
+                </div>
+            </div>
+            <div>
+                <div className="featured-speaker-image">
+                    <img src="/img/summit/vancouver-2023/Silona-Bonewald.jpeg" />
+                </div>
+                <div className="featured-speaker-info">
+                    <div className="featured-speaker-name">Silona Bonewald</div>
+                    <div className="featured-speaker-company">IEEE. SA OPEN</div>
                 </div>
             </div>
         </div>

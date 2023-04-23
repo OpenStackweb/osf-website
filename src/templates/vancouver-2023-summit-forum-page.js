2a3734021805f56215dbf423870145e3038f228b
Josh Lohse <yoshwaaa@gmail.com>
2023-04-23 07:19:36 -0400

    Removed forum links

--- src/templates/vancouver-2023-summit-forum-page.js
+++ src/templates/vancouver-2023-summit-forum-page.js
@@ -29,10 +29,10 @@ export const SummitForumPageTemplate = ({
         <NavbarV2 isLoggedUser={isLoggedUser} />
         <SubNavYvr active="summit-forum" pageName="Forum" isLoggedUser={isLoggedUser}/>
         <Header title={title} subTitle={subTitle} />
-        <div className="container summit-track-subtitle-container">
+        {/* <div className="container summit-track-subtitle-container">
           <a href="https://cfp.openinfra.dev/app/vancouver-2023/20" class="button-cta" target="_blank" rel="noopener noreferrer">Submit a Forum Discussion<img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTUiIGhlaWdodD0iMTIiIHZpZXdCb3g9IjAgMCAxNSAxMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayI+DQogICAgPGRlZnM+DQogICAgICAgIDxwYXRoIGQ9Ik03MDguNzE2IDM0OS4wNTRsLTQuODA1LTQuODA2YS44MjguODI4IDAgMCAwLTEuMTgzIDAgLjgyOC44MjggMCAwIDAgMCAxLjE4MmwzLjM4OCAzLjM4N2gtMTEuMjgxYS44NC44NCAwIDAgMC0uODM1LjgzNi44NC44NCAwIDAgMCAuODM1LjgzNWgxMS4yNjZsLTMuMzczIDMuMzg3YS44MjguODI4IDAgMCAwIDAgMS4xODIuODYzLjg2MyAwIDAgMCAuNi4yNTJjLjIyIDAgLjQyNS0uMDc5LjU5OS0uMjUybDQuODItNC44MjJhLjg1OC44NTggMCAwIDAgLjI1Mi0uNTk4IDEuMDY5IDEuMDY5IDAgMCAwLS4yODMtLjU4MyIgaWQ9ImEtYXJyb3ctbGVmdCIgLz4NCiAgICA8L2RlZnM+DQogICAgPGcgdHJhbnNmb3JtPSJ0cmFuc2xhdGUoLTY5NCAtMzQ0KSI+DQogICAgICAgIDx1c2UgeGxpbms6aHJlZj0iI2EtYXJyb3ctbGVmdCIgZmlsbD0iI2ZmZiIgLz4NCiAgICA8L2c+DQo8L3N2Zz4=" alt="" /></a>
           <p>Discussions can be 30 or 70 minutes</p>
-        </div>
+        </div> */}
       </div>
 
       <main className="main">

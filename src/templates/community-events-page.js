257b88ad4423cdf2e735c3974cff44272bbb3e9b
Josh Lohse <yoshwaaa@gmail.com>
2023-08-23 17:40:16 -0400

    Changed positioning of the the newsletter form on events page

--- src/templates/community-events-page.js
+++ src/templates/community-events-page.js
@@ -38,25 +38,25 @@ export const CommunityEventsPageTemplate = ({
               <div className="columns">
                 <div className="column community-events-wrapper">
                   <PageContent content={content} />
-                  <section className="summit-form ptg">
-                  <div className="summit-form-container ptg">
-                    <div className="form-columns">
-                        <div className="form-left">
-                            <div className="picture">
-                                <img alt="form image" src="/static/bce6b8aa2b64717a88febc9f2b3af2ba/23495/EWP-_WP_5140e%201.png" />
-                            </div>
-                        </div>
-                        <div class="form-right">
-                            <p className="description">Subscribe to our newsletter to be kept up to date with the latest about the Summit.</p>
-                            <div>
-                                <a href="/newsletter" class="form-cta" target="_blank" rel="noopener noreferrer">Sign Up <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTUiIGhlaWdodD0iMTIiIHZpZXdCb3g9IjAgMCAxNSAxMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayI+CiAgICA8ZGVmcz4KICAgICAgICA8cGF0aCBkPSJNNzA4LjcxNiAzNDkuMDU0bC00LjgwNS00LjgwNmEuODI4LjgyOCAwIDAgMC0xLjE4MyAwIC44MjguODI4IDAgMCAwIDAgMS4xODJsMy4zODggMy4zODdoLTExLjI4MWEuODQuODQgMCAwIDAtLjgzNS44MzYuODQuODQgMCAwIDAgLjgzNS44MzVoMTEuMjY2bC0zLjM3MyAzLjM4N2EuODI4LjgyOCAwIDAgMCAwIDEuMTgyLjg2My44NjMgMCAwIDAgLjYuMjUyYy4yMiAwIC40MjUtLjA3OS41OTktLjI1Mmw0LjgyLTQuODIyYS44NTguODU4IDAgMCAwIC4yNTItLjU5OCAxLjA2OSAxLjA2OSAwIDAgMC0uMjgzLS41ODMiIGlkPSJhLWFycm93LWxlZnQiIC8+CiAgICA8L2RlZnM+CiAgICA8ZyB0cmFuc2Zvcm09InRyYW5zbGF0ZSgtNjk0IC0zNDQpIj4KICAgICAgICA8dXNlIHhsaW5rOmhyZWY9IiNhLWFycm93LWxlZnQiIGZpbGw9IiNmZmYiIC8+CiAgICA8L2c+Cjwvc3ZnPg==" alt="" /></a>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
-                  </section>
                 </div>
               </div>
+            </div>
+          </section>
+          <section className="summit-form ptg">
+            <div className="summit-form-container ptg">
+            <div className="form-columns">
+                <div className="form-left">
+                    <div className="picture">
+                        <img alt="form image" src="/static/bce6b8aa2b64717a88febc9f2b3af2ba/23495/EWP-_WP_5140e%201.png" />
+                    </div>
+                </div>
+                <div class="form-right">
+                    <p className="description">Subscribe to our newsletter to be kept up to date with the latest about the Summit.</p>
+                    <div>
+                        <a href="/newsletter" class="form-cta" target="_blank" rel="noopener noreferrer">Sign Up <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTUiIGhlaWdodD0iMTIiIHZpZXdCb3g9IjAgMCAxNSAxMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayI+CiAgICA8ZGVmcz4KICAgICAgICA8cGF0aCBkPSJNNzA4LjcxNiAzNDkuMDU0bC00LjgwNS00LjgwNmEuODI4LjgyOCAwIDAgMC0xLjE4MyAwIC44MjguODI4IDAgMCAwIDAgMS4xODJsMy4zODggMy4zODdoLTExLjI4MWEuODQuODQgMCAwIDAtLjgzNS44MzYuODQuODQgMCAwIDAgLjgzNS44MzVoMTEuMjY2bC0zLjM3MyAzLjM4N2EuODI4LjgyOCAwIDAgMCAwIDEuMTgyLjg2My44NjMgMCAwIDAgLjYuMjUyYy4yMiAwIC40MjUtLjA3OS41OTktLjI1Mmw0LjgyLTQuODIyYS44NTguODU4IDAgMCAwIC4yNTItLjU5OCAxLjA2OSAxLjA2OSAwIDAgMC0uMjgzLS41ODMiIGlkPSJhLWFycm93LWxlZnQiIC8+CiAgICA8L2RlZnM+CiAgICA8ZyB0cmFuc2Zvcm09InRyYW5zbGF0ZSgtNjk0IC0zNDQpIj4KICAgICAgICA8dXNlIHhsaW5rOmhyZWY9IiNhLWFycm93LWxlZnQiIGZpbGw9IiNmZmYiIC8+CiAgICA8L2c+Cjwvc3ZnPg==" alt="" /></a>
+                    </div>
+                </div>
+            </div>
             </div>
           </section>
           {footer &&

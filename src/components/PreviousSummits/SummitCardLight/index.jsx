aeefc47bc1404e9373c274c66b56d70510bd9ba8
Santiago Palenque <santipalenque@gmail.com>
2023-12-12 18:09:34 -0300

    previous summits done

--- src/components/PreviousSummits/SummitCardLight/index.jsx
+++ src/components/PreviousSummits/SummitCardLight/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import {StaticImage} from "gatsby-plugin-image";
 
-import {wrapper, image, title, subtitle, button} from './styles.module.scss';
+import './styles.scss';
 
 const SummitCardLight = ({summit}) => {
   return (
-    <div className={wrapper}>
-      {/*<StaticImage className={image} src={summit.image} alt={summit.name} />*/}
-      <p className={title}>{summit.name}</p>
-      <p className={subtitle}>{summit.date}</p>
-      <a href={summit.link} className={button}>See Highlights --> </a>
+    <div className="summit-card-wrapper">
+      <img className="image" alt={summit.name} src={summit.image} />
+      <p className="title">{summit.name}</p>
+      <p className="subtitle">{summit.date}</p>
+      <a href={summit.link} className="button">
+        See Highlights <img src="img/right-arrow.svg" alt="right arrow" />
+      </a>
     </div>
   )
 }
--- src/components/PreviousSummits/index.jsx
+++ src/components/PreviousSummits/index.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
-import {StaticImage} from "gatsby-plugin-image";
-
-import {wrapper, image, title, subtitle, button} from './styles.module.scss';
 import SummitCardLight from "./SummitCardLight";
+import CustomSlider from "../CustomSlider";
+
+import './styles.scss';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const PREVIOUS_SUMMITS = [
-  {name: 'Vancouver, Canada', date: 'June 13-15, 2023', image: '../img/summits/vancouver.png', link: ''},
-  {name: 'Berlin, Germany', date: 'June 7-9, 2022', image: '../img/summits/berlin.png', link: ''},
-  {name: 'Denver, Colorado', date: 'April 29 - May 1, 2019', image: '../img/summits/denver.png', link: ''},
-  {name: 'Shanghai, China', date: 'November 4-6, 2019', image: '../img/summits/shanghai.png', link: ''}
+  {name: 'Vancouver, Canada', date: 'June 13-15, 2023', image: 'img/summit-landing/summits/vancouver.png', link: ''},
+  {name: 'Berlin, Germany', date: 'June 7-9, 2022', image: 'img/summit-landing/summits/berlin.png', link: ''},
+  {name: 'Denver, Colorado', date: 'April 29 - May 1, 2019', image: 'img/summit-landing/summits/denver.png', link: ''},
+  {name: 'Shanghai, China', date: 'November 4-6, 2019', image: 'img/summit-landing/summits/shanghai.png', link: ''}
 ];
 
 const PreviousSummits = ({}) => {
   return (
-    <div className={wrapper}>
-      {PREVIOUS_SUMMITS.map(summit => <SummitCardLight summit={summit} key={summit.name} />)}
+    <div className="container previous-summits-wrapper">
+      <CustomSlider title="Previous OpenInfra Foundation Summits">
+        {PREVIOUS_SUMMITS.map(summit =>
+          <SummitCardLight summit={summit} key={summit.name}/>
+        )}
+      </CustomSlider>
     </div>
   )
 }

d0f8e230034fbcb9bb44a086bd17e85064078b6c
Santiago Palenque <santipalenque@gmail.com>
2023-12-13 17:49:44 -0300

    fix images

--- src/components/PreviousSummits/index.jsx
+++ src/components/PreviousSummits/index.jsx
@@ -7,10 +7,10 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const PREVIOUS_SUMMITS = [
-  {name: 'Vancouver, Canada', date: 'June 13-15, 2023', image: 'img/summit-landing/summits/vancouver.png', link: ''},
-  {name: 'Berlin, Germany', date: 'June 7-9, 2022', image: 'img/summit-landing/summits/berlin.png', link: ''},
-  {name: 'Denver, Colorado', date: 'April 29 - May 1, 2019', image: 'img/summit-landing/summits/denver.png', link: ''},
-  {name: 'Shanghai, China', date: 'November 4-6, 2019', image: 'img/summit-landing/summits/shanghai.png', link: ''}
+  {name: 'Vancouver, Canada', date: 'June 13-15, 2023', image: '/img/summit-landing/summits/vancouver.png', link: ''},
+  {name: 'Berlin, Germany', date: 'June 7-9, 2022', image: '/img/summit-landing/summits/berlin.png', link: ''},
+  {name: 'Denver, Colorado', date: 'April 29 - May 1, 2019', image: '/img/summit-landing/summits/denver.png', link: ''},
+  {name: 'Shanghai, China', date: 'November 4-6, 2019', image: '/img/summit-landing/summits/shanghai.png', link: ''}
 ];
 
 const PreviousSummits = () => {

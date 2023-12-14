b1efc50c981e9d8e6f85fdf7290c26e648dc556d
Tomás Castillo <tcastilloboireau@gmail.com>
2023-12-14 14:40:16 -0300

    Add props to bottom banner, add links to components
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/MeetupBanner/index.jsx
+++ src/components/MeetupBanner/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import background from '../../../static/img/summit-landing/meetup/meetup-banner.png'
 import openinfraUserGroup from '../../../static/img/summit-landing/meetup/openinfra-usergroup.svg'
+import LinkComponent from '../LinkComponent'
 
 import './styles.scss'
 
@@ -12,9 +13,9 @@ const MeetupBanner = () => {
                 <div className='meetup-banner-content'>
                     <img src={openinfraUserGroup} />
                     <div>
-                        <a href="/" className="meetup-banner-button">
+                        <LinkComponent href="https://www.meetup.com/pro/openinfradev/" target='_blank' className="meetup-banner-button">
                             <span className="btn-arrow">find your local meetup</span>
-                        </a>
+                        </LinkComponent>
                         <span>
                             Interested in hosting a Meetup? <br/>
                             Contact us at <a href='mailto:events@openinfra.dev'>events@openinfra.dev</a>

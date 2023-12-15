8abfd5fc45aca95098f9aad455137e8678e7e149
Tomás Castillo <tcastilloboireau@gmail.com>
2023-12-15 17:02:53 -0300

    Add new generic banner, add bottom banner
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

---
+++ src/components/GenericBanner/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './styles.scss'
+
+const GenericBanner = ({ upperText, text, button }) => {
+    return (
+        <section className='generic-banner-wrapper'>
+            <div className="generic-banner">
+                <div className='generic-banner-content'>
+                    <span className='generic-banner-upper-text' dangerouslySetInnerHTML={{ __html: upperText }} />
+                    <span className='generic-banner-text' dangerouslySetInnerHTML={{ __html: text }} />
+                    <a href={button.link} className="generic-banner-button">
+                        <span className="btn-arrow">{button.text}</span>
+                    </a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default GenericBanner

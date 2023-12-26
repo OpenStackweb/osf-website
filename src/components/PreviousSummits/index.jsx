f5bdc439d467171cd46564eb6a121a6d4dc24584
Santiago Palenque <santipalenque@gmail.com>
2023-12-26 11:38:22 -0300

    update links

--- src/components/PreviousSummits/index.jsx
+++ src/components/PreviousSummits/index.jsx
@@ -9,8 +9,8 @@ import "slick-carousel/slick/slick-theme.css";
 const PREVIOUS_SUMMITS = [
   {name: 'Vancouver, Canada', date: 'June 13-15, 2023', image: '/img/summit-landing/summits/vancouver.png', link: '/summit/vancouver-2023'},
   {name: 'Berlin, Germany', date: 'June 7-9, 2022', image: '/img/summit-landing/summits/berlin.png', link: '/summit/berlin-2022'},
-  {name: 'Denver, Colorado', date: 'April 29 - May 1, 2019', image: '/img/summit-landing/summits/denver.png', link:  '/summit/denver-2019'},
-  {name: 'Shanghai, China', date: 'November 4-6, 2019', image: '/img/summit-landing/summits/shanghai.png', link:  '/summit/shanghai-2019'}
+  {name: 'Denver, Colorado', date: 'April 29 - May 1, 2019', image: '/img/summit-landing/summits/denver.png', link:  'https://www.openstack.org/summit/denver-2019'},
+  {name: 'Shanghai, China', date: 'November 4-6, 2019', image: '/img/summit-landing/summits/shanghai.png', link:  'https://www.openstack.org/summit/shanghai-2019'}
 ];
 
 const PreviousSummits = () => {

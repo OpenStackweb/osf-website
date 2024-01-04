e12f469c2729610e306194b3acae98b11ad81b63
Tomás Castillo <tcastilloboireau@gmail.com>
2024-01-04 13:51:29 -0300

    Changes in sponsorship header, change text on bottom banner
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/SimpleHeader/index.jsx
+++ src/components/SimpleHeader/index.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 
 import './styles.scss';
+import LinkComponent from '../LinkComponent';
 
-const SimpleHeader = ({title, subtitle, backgroundImage}) => {
+const SimpleHeader = ({title, subtitle, backgroundImage, button}) => {
+  console.log('button...', button);
   return (
     <main className="main">
       <section className="hero-main-v2 simple-header" style={{backgroundImage: `url(${backgroundImage})`}}>
         <div className="container">
           <h1 className="title">{title}</h1>
-          <p className="subtitle">{subtitle}</p>
+          <p className="subtitle" dangerouslySetInnerHTML={{__html: subtitle}}/>
+          {button.text &&
+            <LinkComponent href={button.link} className="banner-button">
+              <span className="btn-arrow">{button.text}</span>
+            </LinkComponent>
+          }
         </div>
       </section>
     </main>

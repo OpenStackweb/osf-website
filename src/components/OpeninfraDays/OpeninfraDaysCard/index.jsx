d9eaaec62a9a90e0a26c78662c1366407626c923
Tomás Castillo <tcastilloboireau@gmail.com>
2024-01-18 14:09:21 -0300

    Add link to openinfra card component
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/OpeninfraDays/OpeninfraDaysCard/index.jsx
+++ src/components/OpeninfraDays/OpeninfraDaysCard/index.jsx
@@ -6,25 +6,28 @@ import locationIcon from '../../../../static/img/summit-landing/icons/location-i
 import logo from '../../../../static/img/openinfra-days/openinfra-days-cards/openinfra-days-logo.png'
 
 import './styles.scss';
+import LinkComponent from '../../LinkComponent';
 
-const OpenInfraDaysCard = ({ background, name, date, location }) => {
+const OpenInfraDaysCard = ({ background, name, date, location, link = '#' }) => {
   return (
-    <div className="openinfra-days-card-wrapper">
-      <div className="top" style={{ backgroundImage: `url(${background})` }}>
-        <img src={logo} />
-        <span className="title">{name}</span>
-      </div>
-      <div className="info">
-        <div className='info-wrapper'>
-          <span>
-            <img src={calendarIcon} /> {date}
-          </span>
-          <span>
-            <img src={locationIcon} /> {location}
-          </span>
+    <LinkComponent href={link}>
+      <div className="openinfra-days-card-wrapper">
+        <div className="top" style={{ backgroundImage: `url(${background})` }}>
+          <img src={logo} />
+          <span className="title">{name}</span>
         </div>
-      </div>
-    </div >
+        <div className="info">
+          <div className='info-wrapper'>
+            <span>
+              <img src={calendarIcon} /> {date}
+            </span>
+            <span>
+              <img src={locationIcon} /> {location}
+            </span>
+          </div>
+        </div>
+      </div >
+    </LinkComponent>
   )
 }
 

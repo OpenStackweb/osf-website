99f5289cba5a1d83817ef4a5d1410d43e454e7af
Tomás Castillo <tcastilloboireau@gmail.com>
2024-01-18 18:34:31 -0300

    Adjust name on openinfra days card
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/OpeninfraDays/index.jsx
+++ src/components/OpeninfraDays/index.jsx
@@ -6,7 +6,7 @@ import './styles.scss';
 const OpenInfraDays = ({ title }) => {
 
   const openinfraEvents = [    
-    { background: '/img/openinfra-days/openinfra-days-cards/openinfra-days-austin.png', name: 'OpenInfra Days USA', date: 'Q4 2024', location: 'More info coming soon!', link: 'https://openinfrafoundation.formstack.com/forms/2024_openinfra_events_updates'}
+    { background: '/img/openinfra-days/openinfra-days-cards/openinfra-days-austin.png', name: 'USA', date: 'Q4 2024', location: 'More info coming soon!', link: 'https://openinfrafoundation.formstack.com/forms/2024_openinfra_events_updates'}
   ]
 
   return (

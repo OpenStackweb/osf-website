baf5da8e8f9013e737ecca59f127077fea7031db
Wes Wilson <wes@iamweswilson.com>
2024-02-29 17:08:38 -0800

    revised project descriptions and intro

--- src/components/UniverseListing/index.jsx
+++ src/components/UniverseListing/index.jsx
@@ -4,17 +4,17 @@ import LinkComponent from '../LinkComponent';
 
 
 const UNIVERSE = [
-  {name: "Airship", link: "https://airshipit.org", logo: "airship.svg", description: "Airship is a collection of loosely coupled but interoperable open source tools.", tags: ["cloud provisioning"], isOIF: true},
-  {name: "Ceph", link: "https://ceph.io", logo: "ceph.svg", description: "Ceph is a software-defined storage platform that provides object storage, block storage, and file storage.", tags: ["storage"], isOIF: false},
-  {name: "DPDK", link: "https://dpdk.org", logo: "dpdk.svg", description: "DPDK consists of libraries to accelerate packet processing workloads running on a wide variety of CPU.", tags: ["packet processing"], isOIF: false},
-  {name: "Exosphere", link: "https://exosphere.app", logo: "exosphere.png", description: "A user-friendly, extensible OpenStack client.", tags: ["front-end client"], isOIF: false},
-  {name: "Kata Containers", link: "https://katacontainers.io", logo: "katacontainers.svg", description: "Kata Containers is a container runtime, building lightweight VMs that seamlessly plug into the containers ecosystem.", tags: ["container runtime"], isOIF: true},
-  {name: "Kubernetes", link: "https://kubernetes.io", logo: "kubernetes.png", description: "Kubernetes is a container orchestration system for automating software deployment, scaling, and management.", tags: ["container orchestration"], isOIF: false},
-  {name: "openEuler", link: "https://www.openeuler.org/", logo: "openeuler.png", description: "openEuler is an open source, free Linux distribution platform. The platform provides an open community for global developers to build an open, diversified, and architecture-inclusive software ecosystem.", tags: ["OS distribution"], isOIF: false},
-  {name: "OpenStack", link: "https://openstack.org", logo: "openstack.svg", description: "OpenStack is a free, open standard cloud computing platform, controlling large pools of compute, storage, and networking resources, all managed through APIs or a dashboard.", tags: ["Compute", "Storage", "Networking"], isOIF: true},
-  {name: "Sovereign Cloud Stack", link: "https://scs.community", logo: "scs.svg", description: "Sovereign Cloud Stack combines the best of cloud computing in one unified standard. SCS is built, backed, and operated by an active open-source community.", tags: ["IaaS"], isOIF: false},
-  {name: "StarlingX", link: "https://starlingx.io", logo: "starlingx.svg", description: "StarlingX is a complete, open source cloud infrastructure software stack for the edge computing workloads, used by the most demanding applications.", tags: ["Edge Computing"], isOIF: true},
-  {name: "Zuul", link: "https://zuul-ci.org", logo: "zuul.svg", description: "Zuul is an open source CI that powers some of the largest Open Source development efforts.", tags: ["continuous integration"], isOIF: true},
+  {name: "Airship", link: "https://airshipit.org", logo: "airship.svg", description: "“A collection of loosely coupled but interoperable tools that declaratively automate cloud provisioning.", tags: ["cloud provisioning"], isOIF: true},
+  {name: "Ceph", link: "https://ceph.io", logo: "ceph.svg", description: "A software-defined storage platform, often used in open infrastructure deployments to provide scalable block storage for OpenStack and others.", tags: ["storage"], isOIF: false},
+  {name: "DPDK", link: "https://dpdk.org", logo: "dpdk.svg", description: "A set of user-space libraries and drivers that accelerate packet-processing workloads.", tags: ["packet processing"], isOIF: false},
+  {name: "Exosphere", link: "https://exosphere.app", logo: "exosphere.png", description: "Provides a user-friendly interface for non-proprietary cloud infrastructure.", tags: ["front-end client"], isOIF: false},
+  {name: "Kata Containers", link: "https://katacontainers.io", logo: "katacontainers.svg", description: "A container runtime with lightweight virtual machines that feel and perform like containers, but provide stronger workload isolation.", tags: ["container runtime"], isOIF: true},
+  {name: "Kubernetes", link: "https://kubernetes.io", logo: "kubernetes.png", description: "An orchestration platform for containerized applications that runs on top of programmable infrastructure and key component of the LOKI stack.", tags: ["container orchestration"], isOIF: false},
+  {name: "openEuler", link: "https://www.openeuler.org/", logo: "openeuler.png", description: "An operating system compatible with multiple computing architectures.", tags: ["OS distribution"], isOIF: false},
+  {name: "OpenStack", link: "https://openstack.org", logo: "openstack.svg", description: "The combination of compute, network and storage resources providing the cloud infrastructure for public, private and hybrid clouds, and a key component of the LOKI stack.", tags: ["Compute", "Storage", "Networking"], isOIF: true},
+  {name: "Sovereign Cloud Stack", link: "https://scs.community", logo: "scs.svg", description: "A network of standardized sovereign cloud and container infrastructure providers defining, implementing and operating a fully open, federated, compatible platform.", tags: ["IaaS"], isOIF: false},
+  {name: "StarlingX", link: "https://starlingx.io", logo: "starlingx.svg", description: "A complete cloud infrastructure software stack for distributed systems created for ultra-low latency use cases.", tags: ["Edge Computing"], isOIF: true},
+  {name: "Zuul", link: "https://zuul-ci.org", logo: "zuul.svg", description: "A program that drives continuous integration, delivery, and deployment systems with a focus on project gating.", tags: ["continuous integration"], isOIF: true},
 ];
 
 const UniverseListing = () => {
@@ -25,7 +25,7 @@ const UniverseListing = () => {
           OpenInfra Universe
         </h1>
         <p className="intro">
-          Open infrastructure goes beyond the projects that are <a href="/projects/">directly hosted at the OpenInfra Foundation</a>. The OpenInfra Universe provides a look at the open source solutions available for infrastructure projects: open source projects that help provide infrastructure resources for others to build on, in use cases such as edge computing, container infrastructure, public/private hybrid cloud, AI & machine learning, CI/CD, and more. Projects in the OpenInfra Universe are discussed at <a href="/community-events/">OpenInfra events</a> around the world. 
+          Open infrastructure goes beyond the projects that are <a href="/projects/">directly hosted at the OpenInfra Foundation</a>. The OpenInfra Universe provides a look at the open source solutions available for infrastructure projects: open source projects that help provide infrastructure resources for others to build on, in use cases such as edge computing, container infrastructure, public/private hybrid cloud, AI & machine learning, CI/CD, and more. Most of these projects power or rely on LOKI (Linux OpenStack Kubernetes Infrastructure), an end-to-end open source solution for providing infrastructure. Projects in the OpenInfra Universe are discussed at <a href="/community-events/">OpenInfra events</a> around the world. 
         </p>
         <p>
         <a class="button button-red" href="#universe-list">

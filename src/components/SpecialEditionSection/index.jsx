70a02a4fd6c6c50d607103bc9c73ca606e1dfd66
Jimmy McArthur <jimmy@openstack.org>
2024-03-28 13:11:06 -0500

    Marking Switz "sold out"

--- src/components/SpecialEditionSection/index.jsx
+++ src/components/SpecialEditionSection/index.jsx
@@ -37,7 +37,7 @@ const DAYS = [
     title: "OpenInfra Meetup: Switzerland",
     date: "June 6, 2024",
     location: '<a href="https://www.google.com/maps/place/CERN/@46.2337442,6.056322,15.89z/data=!4m6!3m5!1s0x478c62fcec737b11:0x81bef3ae7a885e31!8m2!3d46.2330492!4d6.0556771!16zL20vMDk5cm4?entry=ttu" target="_blank" rel="noopener noreferrer">CERN</a>, Geneva, Switzerland',
-    registration: '<a href="https://indico.cern.ch/e/openinfra-cern-2024" target="_blank" rel="noopener noreferrer">Register Now</a>',
+    registration: '<a href="https://indico.cern.ch/e/openinfra-cern-2024" target="_blank" rel="noopener noreferrer">EVENT SOLD OUT</a>',
   },
   {
     title: "OpenInfra Meetup: UK",

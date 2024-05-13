760fe5576e2370f67ec276e8a6f286d93bfd9b95
Tomás Castillo <tomrndom@users.noreply.github.com>
2024-05-13 17:10:48 -0300

    Fix mobile view on join page, fix headerv2 mobile (#594)
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/ImageOnlyHeader.js
+++ src/components/ImageOnlyHeader.js
@@ -5,7 +5,7 @@ import '../style/modules/_image-only-header.scss'
 const ImageOnlyHeader = ({backgroundImage}) => {
   return (
     <main className="main">
-      <section className="hero-main-v2" style={{backgroundImage: `url(${backgroundImage})`}}>
+      <section className="hero-main-v2 image-only" style={{backgroundImage: `url(${backgroundImage})`}}>
         <div className="container"></div>
       </section>
     </main>

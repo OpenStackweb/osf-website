597efae4634c0a261a8c573021e826f8006e7248
Matias Perrone <matias.perrone@exomindset.co>
2025-08-18 11:59:57 -0300

    Add handle submit encapsulation for captcha forms.

--- src/components/ContactFormVertical.js
+++ src/components/ContactFormVertical.js
@@ -1,16 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Swal from "sweetalert2";
-import URI from 'urijs';
-import {getServerFunctionUrl} from '../utils/functionsUtils';
-import useTurnstileCaptcha, { getErrorCodeToDescription } from './TurnstileCaptcha';
+import React, { useEffect } from 'react';
+import useTurnstileCaptcha from './TurnstileCaptcha';
 
 const ContactFormVertical = () => {
 
-    const turnstileCaptchaFieldName = 'cf-turnstile-response';
-    const [inputs, setInputs] = useState({});
-    const [success, setSuccess] = useState(false);
-    const widget = useRef(null);
-    const { token } = useTurnstileCaptcha({ widget });
+    const { widget, success, inputs, setInputs, handleSubmit, handleChange, } = useTurnstileCaptcha();
 
     const checkLevel = () => {
         let value = '';
@@ -24,95 +17,9 @@ const ContactFormVertical = () => {
         setInputs(values => ({...values, membership_interest: value}))
     }
 
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({...values, [name]: value}))
-    }
-
     useEffect(() => {
         checkLevel()
     },[]);
-
-    useEffect(() => {
-        setInputs(values => ({ ...values, [turnstileCaptchaFieldName]: token }))
-    }, [token]);
-
-    const handleSubmit = (evt) => {
-        try
-        {
-            evt.target.disabled = true;
-            evt.preventDefault();
-
-            const uri = new URI();
-            uri.addQuery("form-name", evt.target.getAttribute("name"));
-            uri.addQuery(inputs);
-            if (!uri.hasQuery(turnstileCaptchaFieldName)) {
-                Swal.fire("Validation Error", 'Captcha solution is invalid!.', "warning");
-                evt.target.disabled = false;
-                return false;
-            }
-
-            const URL = getServerFunctionUrl('TurnstileCaptchaValidation');
-            console.log("Submitting form with data:", uri.query(), URL);
-            fetch(
-                URL,
-                {
-                    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                    method: "POST",
-                    body: uri.query(),
-                }).then(async (response) => {
-                    if (response.ok) {
-                        Swal.fire("Form submitted successfully", '', "success");
-                        setSuccess(true);
-                        return;
-                    }
-
-                    // Handle different error types
-                    if (response.status === 412) {
-                        const data = await response.json();
-                        Swal.fire("Validation Error", getErrorCodeToDescription(data['error-codes'][0] ?? null), "warning");
-                    } else if (response.status === 400) {
-                        try {
-                            const data = await response.json();
-                            Swal.fire("Validation Error", (data['error-codes'][0] ?? false) ? getErrorCodeToDescription(data['error-codes'][0]) : 'Invalid form submission', "warning");
-                        } catch(error) {
-                            console.error("Error parsing response:", error);
-                            const text = await response.text();
-                            Swal.fire("Validation Error", text || 'Invalid form submission', "warning");
-                        }
-                    } else if (response.status === 500) {
-                        try {
-                            const data = await response.json();
-                            Swal.fire("Server Error", data.message || 'Internal server error', "error");
-                        } catch(error) {
-                            console.error("Error parsing response:", error);
-                            Swal.fire("Server Error", 'Internal server error', "error");
-                        }
-                    } else {
-                        const text = await response.text();
-                        console.error("Unexpected response code:", response.status);
-                        Swal.fire("Error", text || 'Something went wrong', "warning");
-                    }
-                    setSuccess(false);
-                }).catch(e => {
-                    setSuccess(false);
-                    console.error("Error submitting form:", e);
-                    Swal.fire("Error", 'Oops! Something went wrong.', "warning");
-                });
-
-            return false
-        }
-        catch (e) {
-            setSuccess(false);
-            console.error("Error submitting form", e);
-            Swal.fire("Error", "Oops! Something went wrong.", "warning");
-            return false;
-        }
-        finally {
-            evt.target.disabled = false;
-        }
-    }
 
     return (
         <form className="contact-form"
--- src/components/ProjectsContactForm.js
+++ src/components/ProjectsContactForm.js
@@ -1,34 +1,16 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Dropdown } from 'openstack-uicore-foundation/lib/components'
-import Swal from "sweetalert2";
-import URI from 'urijs';
-import useTurnstileCaptcha, { getErrorCodeToDescription } from './TurnstileCaptcha';
-import { getServerFunctionUrl } from '../utils/functionsUtils';
+import React, { useState, useEffect, } from 'react';
+import { Dropdown } from 'openstack-uicore-foundation/lib/components';
+import useTurnstileCaptcha from './TurnstileCaptcha';
 
 const ProjectsContactForm = ({ privacyPolicyAgreement, successMessage, platinumMembers }) => {
-    const turnstileCaptchaFieldName = 'cf-turnstile-response';
-
-    const [inputs, setInputs] = useState({});
-    const [success, setSuccess] = useState(false);
-    const widget = useRef(null);
-    const { token } = useTurnstileCaptcha({ widget });
+    const { widget, success, inputs, setInputs, handleSubmit, handleChange, } = useTurnstileCaptcha();
 
     const [platinumDropdown, setPlatinumDropdown] = useState([])
-
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({ ...values, [name]: value }))
-    }
 
     const handleTitleChange = () => {
         const title = document.getElementById('title');
         setInputs(values => ({ ...values, title: title.value }))
     }
-
-    useEffect(() => {
-        setInputs(values => ({ ...values, [turnstileCaptchaFieldName]: token }))
-    }, [token]);
 
     useEffect(() => {
         const formattedMembers = platinumMembers?.map(p => {
@@ -43,82 +25,6 @@ const ProjectsContactForm = ({ privacyPolicyAgreement, successMessage, platinumM
 
     const handleDropdownChange = (ev) => {
         setInputs({ ...inputs, project_platinum_member: ev.target.value })
-    }
-
-    const handleSubmit = (evt) => {
-        try {
-            evt.target.disabled = true;
-            evt.preventDefault();
-
-            const uri = new URI();
-            uri.addQuery("form-name", evt.target.getAttribute("name"));
-            uri.addQuery(inputs);
-            if (!uri.hasQuery(turnstileCaptchaFieldName)) {
-                Swal.fire("Validation Error", 'Captcha solution is invalid!.', "warning");
-                evt.target.disabled = false;
-                return false;
-            }
-
-            const URL = getServerFunctionUrl('TurnstileCaptchaValidation');
-            console.log("Submitting form with data:", uri.query(), URL);
-            fetch(
-                URL,
-                {
-                    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                    method: "POST",
-                    body: uri.query(),
-                }).then(async (response) => {
-                    if (response.ok) {
-                        Swal.fire("Form submitted successfully", '', "success");
-                        setSuccess(true);
-                        return;
-                    }
-
-                    // Handle different error types
-                    if (response.status === 412) {
-                        const data = await response.json();
-                        Swal.fire("Validation Error", getErrorCodeToDescription(data['error-codes'][0] ?? null), "warning");
-                    } else if (response.status === 400) {
-                        try {
-                            const data = await response.json();
-                            Swal.fire("Validation Error", (data['error-codes'][0] ?? false) ? getErrorCodeToDescription(data['error-codes'][0]) : 'Invalid form submission', "warning");
-                        } catch (error) {
-                            console.error("Error parsing response:", error);
-                            const text = await response.text();
-                            Swal.fire("Validation Error", text || 'Invalid form submission', "warning");
-                        }
-                    } else if (response.status === 500) {
-                        try {
-                            const data = await response.json();
-                            Swal.fire("Server Error", data.message || 'Internal server error', "error");
-                        } catch (error) {
-                            console.error("Error parsing response:", error);
-                            Swal.fire("Server Error", 'Internal server error', "error");
-                        }
-                    } else {
-                        const text = await response.text();
-                        console.error("Unexpected response code:", response.status);
-                        Swal.fire("Error", text || 'Something went wrong', "warning");
-                    }
-                    setSuccess(false);
-                }).catch(e => {
-                    setSuccess(false);
-                    console.error("Error submitting form:", e);
-                    Swal.fire("Error", 'Oops! Something went wrong.', "warning");
-                });
-
-            return false
-        }
-        catch (e) {
-            setSuccess(false);
-            console.error("Error submitting form", e);
-            Swal.fire("Error", "Oops! Something went wrong.", "warning");
-            return false;
-        }
-        finally {
-            evt.target.disabled = false;
-        }
-
     }
 
     return (
